8b93bf53d726349f39fe84f3d721e135dca49ef5
Gauri Bhand <htcgauribhand@gmail.com>
2022-01-30 12:38:36 +0530

    first

---
+++ 1) 1D ARRAYS/sumofSubArray.cpp
@@ -0,0 +1,22 @@
+//given an array[] of size n.Output sum of each subarray of the given array.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    int curr=0;
+    for(int i=0; i<n; i++){
+        curr=0;
+        for(int j=i; j<n; j++){
+            curr+=a[j];
+            cout<<curr<<endl;
+        }
+
+    }
+     return 0;
+}
