b5de58b4de1d07f24ce2ab1c44400740af2c47e9
Gauri Bhand <htcgauribhand@gmail.com>
2022-01-31 17:11:51 +0530

    first

---
+++ 4) Pattern Questions/Inverted_Half_Pyramid.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i, j;
+    for(i=0; i<6; i++){
+        for(j=i; j<6; j++)
+        cout<<"* ";
+        cout<<endl;
+    }
+    cout<<endl; 
+return 0;
+}
