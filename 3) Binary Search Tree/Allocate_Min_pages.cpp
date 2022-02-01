14950c626d91855bb91a312be1f514f8cca29290
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-01 19:01:49 +0530

    first

---
+++ 3) Binary Search Tree/Allocate_Min_pages.cpp
@@ -0,0 +1,63 @@
+/*n = number of pages in n diffrent books
+m = number of students
+All the books have to be devided among m students, consecatively. 
+Allocate the pages in such a way that maximum pages allocated to the student is minimun
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPossible(int arr[],int n, int m, int min){
+    int studentsRequired = 1, sum = 0;
+
+    for(int i=0; i<n; i++){
+        if(arr[i]>min){
+            return false;
+        }
+        if(sum+arr[i]>min){
+            studentsRequired++;
+            sum = arr[i];
+
+            if(studentsRequired>m){
+                return false;
+            }
+        }
+        else{
+            sum += arr[i];
+        }
+    }
+    return true;
+}
+
+int allocateMin(int arr[], int n, int m){
+    int sum = 0;
+    if(n<m){
+        return -1;
+    }
+    for(int i=0; i<n; i++){
+        sum = sum + arr[i];
+    }
+
+    int start = 0, end = sum, ans = INT_MAX;
+    while(start<=end){
+        int mid = (start+end)/2;
+        if(isPossible(arr, n , m, mid)){
+            ans = min(ans, mid);
+            end = mid-1;
+        }
+        else{
+            start = mid +1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    int arr[] = {12, 34, 67, 90};
+    int n = 4;
+    int m = 2;
+    cout<<"The min no. of pages: "<<allocateMin(arr, n, m)<<endl;
+    
+return 0;
+}
