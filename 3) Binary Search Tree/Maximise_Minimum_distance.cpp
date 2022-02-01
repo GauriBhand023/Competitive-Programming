0f0fa0d83f4844fe4bca464c701ada974d25689b
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-01 18:18:49 +0530

    first

---
+++ 3) Binary Search Tree/Maximise_Minimum_distance.cpp
@@ -0,0 +1,48 @@
+/* Given is an array with n elements that represents n positions along a straight line.
+Find K elements such that the minimum distance between any two elements is the maximum possible.*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isFeasible(int mid, int arr[], int n, int k){
+    int pos = arr[0], elements = 1;
+
+    for(int i=1; i<n; i++){
+       if(arr[i]-pos>=mid){
+           pos = arr[i];
+           elements++;
+           if(elements == k){
+               return true;
+           }
+       }
+    }
+    return false;
+}
+
+int largestMinDist(int arr[], int n, int k){
+    sort(arr, arr+n);
+    int result = -1;
+    int left =1;
+    int right = arr[n-1];
+
+    while(left<right){
+        int mid = (left+right)/2;
+        if(isFeasible(mid, arr, n, k)){
+            result = max(result, mid);
+            left = mid+1;
+        }
+        else{
+            right = mid;
+        }
+    }
+    return result;
+}
+int main()
+{
+    int arr[] = {1,2,8,4,9};
+    int n = 5;
+    int k = 3;
+    cout<<"largest min distance is: "<<largestMinDist(arr, n ,k)<<endl;
+    
+return 0;
+}
