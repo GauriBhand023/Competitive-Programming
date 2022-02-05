2bfb4a49e1cace2f43033bb9a1a6717a31947549
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-05 13:01:54 +0530

    first

---
+++ LEETCODE/Valid_Parentheses.cpp
@@ -0,0 +1,30 @@
+/*Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+ determine if the input string is valid.
+
+An input string is valid if:
+1)Open brackets must be closed by the same type of brackets.
+2)Open brackets must be closed in the correct order.*/
+
+class Solution{
+public:
+    bool isValid(string s){
+    int n = s.length();
+    stack<char>ss;
+    for(int i=0; i<n; i++){
+        if(ss.empty()){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[')ss.push(s[i]);
+            else return false;
+        }
+        else if(s[i] == '(' || s[i] == '{' || s[i] == '[')ss.push(s[i]);
+        else{
+            if(ss.top() == '(' && s[i]==')')ss.pop();
+            if(ss.top() == '[' && s[i]==']')ss.pop();
+            if(ss.top() == '{' && s[i]=='}')ss.pop();
+            else return false;
+        }
+    }
+    if(ss.size() == 0)
+        return true;
+        return false;
+    }
+};
