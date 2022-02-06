eebfc9652c5b083a846c7fd166f3b48a1ba2199b
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-06 10:45:38 +0530

    first

---
+++ 4) Pattern Questions/Hollow_Pattern.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern(int n){
+    int i, j;
+    for(i=1; i<=n; i++){
+        for(j=1; j<=(2*n); j++){
+            if(i>(n-j+1))
+               cout<<" ";
+            else
+               cout<<"*";
+            if((i+n)>j)
+               cout<<" ";
+            else 
+               cout<<"*";
+        }
+        cout<<endl;
+    } 
+    for(i=1; i<=n; i++){
+        for(j=1; j<=(2*n); j++){
+            if(i<j)
+               cout<<" ";
+            else
+               cout<<"*";
+            if(i<=((2*n)-j))
+               cout<<" ";
+            else 
+               cout<<"*";
+        }
+        cout<<endl;
+    }   
+}
+
+
+int main()
+{
+    pattern(7);
+    
+return 0;
+}
