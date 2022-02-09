d59ff56d30407d8b71c125a8413996a944cff6b1
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-09 19:01:57 +0530

    first

---
+++ Codechef/Sum_of_Digits.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+int n;
+cin>>n;
+while(t--){
+  int sum =0;
+  while(n!=0){
+      int y = n%10;
+      sum+= y;
+      n/= 10;
+  }
+  cout<<sum<<endl;
+}
+return 0;
+}
