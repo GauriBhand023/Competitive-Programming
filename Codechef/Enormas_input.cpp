864d98c302ffa37a8d89e03a456029bb3bdf93cd
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-09 18:42:42 +0530

    first

---
+++ Codechef/Enormas_input.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n, k;
+    cin >> n >> k;
+    int cnt=0;
+        for (int i = 0; i <= n-1; i++)
+        {
+            int a;
+            cin >> a;
+            if (a % k == 0)
+            {
+                cnt++;
+            }
+        }
+    
+    cout << cnt << endl;
+    return 0;
+}
