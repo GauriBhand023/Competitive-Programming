f903dedda69c49cda3bbaba52dfb0a32c9cd8a2d
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-10 17:45:43 +0530

    first

---
+++ Codechef/First_last_digits_sum.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  int n;
+  cin>>n;
+  int rem = n%10;
+  while(n>9){
+      n = n/10;
+  }
+  cout<<n+rem<<endl;
+}
+return 0;
+}
