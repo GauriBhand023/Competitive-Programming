604568d1a0f94b2d8dcb3af7a7db46f41d1a2180
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-10 20:19:11 +0530

    first

---
+++ Codechef/Tubo_sort.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        sort(a, a + n);
+        for (int i = 0; i < n; i++)
+            cout << a[i] <<endl;
+    
+    return 0;
+}
