5920cc05bea1f46bb618039116d59447f2ef2d7a
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-10 21:47:15 +0530

    first

---
+++ Codechef/Small_Factorials.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  	    int n;
+	    cin>>n;
+	    int fact=1;
+	    for(int i=n;i>0;i--){
+	     fact=fact*i;
+        }
+	    cout<<fact<<endl; 
+}
+return 0;
+}
