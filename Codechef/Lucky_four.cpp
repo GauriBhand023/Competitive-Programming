7e460656d834765b4ed7e281be0502c71b0efe7b
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-10 17:54:38 +0530

    first

---
+++ Codechef/Lucky_four.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+   int i;
+	    cin >> i;
+	    int count=0;
+	    for(int j=i;j>0;){
+	        if(j%10==4){
+	            count=count+1;
+	        }
+	        j=j/10;
+	    }
+	    cout << count<< endl;
+}
+return 0;
+}
