6d8d63b8b9517ea05142e0b32f7b17f9c0d9acaa
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-10 22:22:23 +0530

    first

---
+++ Codechef/Squart_Root.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	while(n--){
+	    int a;
+	    cin>>a ;
+	    int b = sqrt(a);
+	    cout<<b<<endl;
+	}
+	return 0;
+}
