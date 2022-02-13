fafa695f0428b4954eae375e0363927e0957ac6c
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-13 22:40:12 +0530

    28

---
+++ Codechef/28_Count_substrings.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+	int t;
+	cin>> t;	
+	while(t--){
+	    ll n;
+	    cin>> n;	    
+	    string s;	    
+	    cin>> s;	    
+	    ll count=0;	    
+	    for(ll i=0; i<n; i++){
+	        if(s[i]=='1'){
+	            count++;
+	        }
+	    }	    
+	    ll ans=count*(count+1);
+	    ll ans1=ans/2;	    
+	    cout<< ans1 << "\n";
+	}
+	return 0;
+}
