193e107355f36d4d8146c53967bd882a941d8467
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-13 18:10:00 +0530

    26

---
+++ Codechef/26_uncle_johny.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n, p, g, temp, count = 0;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        cin >> p;
+        p--;
+        g = arr[p];
+        for (int x = 0; x < n; x++)
+        {
+            for (int j = x + 1; j < n; j++)
+            {
+                if (arr[x] > arr[j])
+                {
+                    temp = arr[x];
+                    arr[x] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        int k = 0;
+        while (k < n)
+        {
+            if (arr[k] == g)
+            {
+                break;
+            }
+            k++;
+        }
+        k++;
+        cout << k << endl;
+    }
+}
