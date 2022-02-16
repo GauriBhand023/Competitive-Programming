3f7db2391c3dd7e244552a19c72bee594c600b29
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-16 21:24:01 +0530

    31

---
+++ Codechef/31_GCD_and_LCM.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  long long int x=0, y=0;
+  cin>>x>>y;
+  cout<<__gcd(x,y)<<" "<<((x*y)/__gcd(x,y))<<endl;
+}
+return 0;
+}
