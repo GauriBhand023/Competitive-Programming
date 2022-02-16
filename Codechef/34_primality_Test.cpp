ca6f2bbcebd0d1f8fb6209e1b70cf3d6117e5ed2
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-17 00:00:58 +0530

    34

---
+++ Codechef/34_primality_Test.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool prime(){
+    long long x;
+    cin>>x;
+    if(x==1){
+        return false;
+    }   
+        for (int i = 2; i <= sqrt(x);i++){
+            if (x%i  == 0){
+               return false;
+            }           
+        }   
+    return true;
+}
+int main() {
+int t;
+cin>>t;
+while(t--){
+    if (prime()){
+        cout << "yes"<< endl;
+    }
+    else 
+        cout << "no" << endl;
+}
+return 0;
+}
