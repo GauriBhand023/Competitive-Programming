52f871e693caf3699a0dacea0076eea17b649428
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-24 16:59:57 +0530

    43

---
+++ Codechef/43_Chef_coupon.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    int n=a+a;
+	    int m=b;
+	    int a1,a2,a3,b1,b2,b3;
+	    cin>>a1>>a2>>a3;
+	    cin>>b1>>b2>>b3;
+	    if((a1+a2+a3)<150){
+	        m+=a;
+	    }
+	    if((b1+b2+b3<150)){
+	        m+=a;
+	    }
+	    if(m<n){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
