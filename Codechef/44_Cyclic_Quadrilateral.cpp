670d3b458319ab09fe815c626dcb9f6254082dd0
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-24 17:06:53 +0530

    44

---
+++ Codechef/44_Cyclic_Quadrilateral.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t; 
+	cin>>t;
+    while (t--)
+    {
+        int x,y,a,b,c,d;
+        cin>>a>>b>>c>>d;
+        y=a+c;
+        x=b+d;
+        if(y==180 and x==180){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+    
+}
+
