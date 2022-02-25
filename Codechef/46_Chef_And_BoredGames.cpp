1721d6fe284fa92c4ad147704428ac4d121f7c71
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-25 21:44:19 +0530

    46

---
+++ Codechef/46_Chef_And_BoredGames.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+	    int n;
+	    cin>>n;
+	    int sum=0;
+	    for(int i=1;i<=n;i++)
+	    {
+	        int a;
+	        if(i%2==1)
+	        {
+	            a=i;
+	            sum+=((n-a)+1)*((n-a)+1);
+	        }
+	    }
+	    cout<<sum<<endl;
+	    
+	}
+	return 0;
+}
