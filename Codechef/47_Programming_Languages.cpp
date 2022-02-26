be4c121a5fce805790c82d102604d3056cf8a37a
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-26 13:12:54 +0530

    47

---
+++ Codechef/47_Programming_Languages.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,a1,b1,a2,b2;
+        cin>>a>>b>>a1>>b1>>a2>>b2;
+        if((a1==a&&b1==b)||(a1==b&&b1==a))
+        {
+            cout<<1<<endl;
+        }
+        else if((a2==a&&b2==b)||(a2==b&&b2==a))
+        {
+            cout<<2<<endl;
+        }
+        else
+        {
+            cout<<0<<endl;
+        }
+    }	
+	return 0;
+}
