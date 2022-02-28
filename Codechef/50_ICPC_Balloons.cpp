9c1ab3336d326558e127c83cd0bbf1823b171e67
Gauri Bhand <htcgauribhand@gmail.com>
2022-02-28 21:46:24 +0530

    50

---
+++ Codechef/50_ICPC_Balloons.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        int c = 0;
+        int i;
+        for (i = 0; i < n; i++)
+        {
+            if (a[i] >= 1 && a[i] <= 7)
+                c++;
+            if (c >= 7)
+                break;
+        }
+        cout << i + 1 << endl;
+    }
+
+    return 0;
+}
