af3b40b37e3370a44fc42043f4929180d2b4f2ba
Gauri Bhand <htcgauribhand@gmail.com>
2022-03-04 13:21:10 +0530

    60

---
+++ Codechef/60_Shortest_Path_BT.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    int c=0;
+	    while(a!=b){
+	        c++;
+	        if(a>b){
+	            a/=2;     
+	        }
+	        else{
+	            b/=2;
+	        }
+	    }
+	    cout<<c<<endl;
+	}
+	return 0;
+}
