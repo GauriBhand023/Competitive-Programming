5d5d425ffe9730b9db5848ea8604c7712d8cd700
Gauri Bhand <htcgauribhand@gmail.com>
2022-03-06 17:44:28 +0530

    65

---
+++ Codechef/65_Remove_Element.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        int a[n];
+        for(int i=0;i<n;i++) cin>>a[i];
+        if(n==1) cout<<"YES"<<endl;
+        else{
+            sort(a,a+n);
+            if(a[0]+a[n-1]<=k){
+                cout<<"YES"<<endl;
+            }
+            else cout<<"NO"<<endl;
+        }
+        
+    }
+	return 0;
+}
