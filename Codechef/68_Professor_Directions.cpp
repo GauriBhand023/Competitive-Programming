a558913606f61ae3b0edc7a75fc5040a068b262e
Gauri Bhand <htcgauribhand@gmail.com>
2022-03-06 22:13:33 +0530

    68

---
+++ Codechef/68_Professor_Directions.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  int t;
+  cin>>t;
+  while(t--){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    bool f = false;
+    for(int i = 0; i < n - 1; i++){
+      if(s[i] == 'L' && s[i + 1] == 'L'){
+        f = true;
+        break;
+      }
+      if(s[i] == 'R' && s[i + 1] == 'R'){
+        f = true;
+        break;
+      }
+    }
+    if(f){
+      cout<<"YES\n";
+    }else{
+      cout<<"NO\n";
+    }
+  }
+  return 0;
+}
