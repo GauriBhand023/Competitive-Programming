fa2b0c06f5ee586b29e315233856f329b06317cf
Gauri Bhand <htcgauribhand@gmail.com>
2022-03-07 18:12:44 +0530

    70

---
+++ Codechef/70_Divide_Cake.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define forn(i, e) for (int i = 0; i < e; i++)
+void solve()
+{
+    int a;
+    cin >> a;
+    if (360 % a == 0)
+        cout << 'y' << ' ';
+    else
+        cout << 'n' << ' ';
+    if (a <= 360)
+        cout << 'y' << ' ';
+    else
+        cout << 'n' << ' ';
+    if (a * (a + 1) / 2 <= 360)
+        cout << 'y' << ' ';
+    else
+        cout << 'n' << ' ';
+    cout << endl;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
