534657383b6fa22dd3f57b66ea8bc8a1bcb02fd2
Gauri Bhand <htcgauribhand@gmail.com>
2022-03-08 21:31:38 +0530

    74

---
+++ Codechef/74_Magical_Planks.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,i;
+        int count=1;
+        cin>>n;
+        string str;
+        cin>>str;   
+        for(i=1;i<n;i++) {
+            if(str[i]!=str[i-1]){
+                count+=1;
+            }
+        }
+        cout<<(count)/2<<endl;
+    }
+    
+	return 0;
+}
