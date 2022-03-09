673a75e086e92a1318edc21a343c5d6712a6763a
Gauri Bhand <htcgauribhand@gmail.com>
2022-03-09 16:37:59 +0530

    78

---
+++ Codechef/78_Course_in_University.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define z long long int
+
+void solve(){
+    z n;
+    cin>>n;
+    z arr[n];
+    for(int i=0;i<n;i++) {
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    cout<<(n-arr[n-1])<<endl;
+}
+int main(){
+	z t;
+	cin>>t;
+	while(t--){
+	    solve();
+	}
+	return 0;
+}
