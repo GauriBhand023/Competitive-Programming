0bd90e1da32eaa9121b7d329fee8a9525c58b141
Gauri Bhand <htcgauribhand@gmail.com>
2022-03-12 12:53:03 +0530

    80

---
+++ Codechef/80_Chf_Gift.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+    cin>>t;
+	while(t--){
+	    int n,k;cin>>n>>k;int a[n];
+	    int cnt=0;
+	    for(int i=0;i<n;i++){cin>>a[i];
+	        if(a[i]%2==0)cnt++;}  
+	   
+	  if((k==0)&&(cnt<n)){
+            cout<<"YES"<<endl;
+        }
+        else if((k==0)&&(cnt==n)){
+            cout<<"NO"<<endl;
+        }
+        else if(cnt>=k){
+            cout<<"YES"<<endl;
+        }
+        else if(cnt<k){
+            cout<<"NO"<<endl;
+        }
+	}
+	return 0;
+}
