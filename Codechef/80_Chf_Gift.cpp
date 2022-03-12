6248bbb7a5d430adc48986ee2b3e8e6b4810a0e2
Gauri Bhand <htcgauribhand@gmail.com>
2022-03-12 16:46:42 +0530

    80

--- Codechef/80_Chf_Gift.cpp
+++ Codechef/80_Chf_Gift.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 int main() {
 	int t;
     cin>>t;
@@ -8,8 +7,7 @@ int main() {
 	    int n,k;cin>>n>>k;int a[n];
 	    int cnt=0;
 	    for(int i=0;i<n;i++){cin>>a[i];
-	        if(a[i]%2==0)cnt++;}  
-	   
+	        if(a[i]%2==0)cnt++;}   
 	  if((k==0)&&(cnt<n)){
             cout<<"YES"<<endl;
         }
