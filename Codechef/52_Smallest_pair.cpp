3bf19689eaf33c2f778f01f29470b452e428c52f
Gauri Bhand <htcgauribhand@gmail.com>
2022-03-14 09:19:36 +0530

    80

--- Codechef/52_Smallest_pair.cpp
+++ Codechef/52_Smallest_pair.cpp
@@ -1,16 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
 	int t;
 	cin>>t;
-	while(t--)
-	{
+	while(t--){
 	    int n;
 	    cin>>n;
 	    int a[n];
-	    for(int i=0;i<n;i++)
-	    {
+	    for(int i=0;i<n;i++){
 	        cin>>a[i];
 	    }
 	    sort(a,a+n);
