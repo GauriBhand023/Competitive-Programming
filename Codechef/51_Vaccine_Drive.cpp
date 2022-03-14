de21d642e754a99de4f41b695e2235633961c4c8
Gauri Bhand <htcgauribhand@gmail.com>
2022-03-14 09:22:06 +0530

    80

--- Codechef/51_Vaccine_Drive.cpp
+++ Codechef/51_Vaccine_Drive.cpp
@@ -1,11 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-
- 
+typedef long long ll; 
 int main(){
 ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
 ll t;
 cin>>t;
 while(t--){
