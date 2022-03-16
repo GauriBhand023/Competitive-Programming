6dc7b5fdaaf415039e4d0c94ae64dd2c1d278418
Gauri Bhand <htcgauribhand@gmail.com>
2022-03-16 19:43:32 +0530

    53

--- Codechef/53_chef_Uses_SocialNetwork.cpp
+++ Codechef/53_chef_Uses_SocialNetwork.cpp
@@ -1,58 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-struct post
-{
+struct post{
     bool is_special;
     int popularity;
     string content;
 };
-
-bool compare (post a, post b)
-{
-    if (a.is_special != b.is_special)
-    {
+bool compare (post a, post b){
+    if (a.is_special != b.is_special){
         return a.is_special>b.is_special;
     }
-    else
-    {
+    else{
         return a.popularity>b.popularity;
     }
 }
-
-int main()
-{
+int main(){
     int a,b;
     cin >> a >> b;
-
     vector<int> special(a);
-    for (int i=0;i<=a-1;i++)
-    {
+    for (int i=0;i<=a-1;i++) {
         cin >> special[i];
     }
     sort(special.begin(), special.end());
-    
     vector<post> arr(b);
-    for (int i=0;i<=b-1;i++)
-    {
+    for (int i=0;i<=b-1;i++){
         int f;
         cin >> f;
-        if (binary_search(special.begin(), special.end(), f))
-        {
+        if (binary_search(special.begin(), special.end(), f)){
             arr[i].is_special=true;
         }
-        else
-        {
+        else {
             arr[i].is_special=false;
         }
         cin >> arr[i].popularity >> arr[i].content;
     }
-
     sort(arr.begin(), arr.end(), compare);
-    for (int i=0;i<=b-1;i++)
-    {
+    for (int i=0;i<=b-1;i++){
         cout << arr[i].content << endl;
     }
-
     return 0;
 }
