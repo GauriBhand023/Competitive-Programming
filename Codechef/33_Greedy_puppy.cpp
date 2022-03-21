e6da19ad1659cef7861757107698bedfc57abd73
Gauri Bhand <htcgauribhand@gmail.com>
2022-03-22 00:06:17 +0530

    33

--- Codechef/33_Greedy_puppy.cpp
+++ Codechef/33_Greedy_puppy.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 typedef long long ll;
 int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
 int t;cin>>t;
 while(t--){
   int n,k;cin>>n>>k;
