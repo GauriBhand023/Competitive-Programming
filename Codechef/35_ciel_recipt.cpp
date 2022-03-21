2e965b845441b67a5c669791cfd1edbe324085f5
Gauri Bhand <htcgauribhand@gmail.com>
2022-03-21 14:15:55 +0530

    35

--- Codechef/35_ciel_recipt.cpp
+++ Codechef/35_ciel_recipt.cpp
@@ -1,16 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
+int main(){
     int t;
     cin>>t;
-    while(t--)
-    {
+    while(t--){
         int p;
         cin>>p;
         int price=2048,count=0;
-        while(p>0)
-        {
+        while(p>0){
             count=count+(p/price);
             p=p%price;
             price=price/2;
