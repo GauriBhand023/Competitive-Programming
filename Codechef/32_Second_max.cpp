1177ab04842a1bad5811cfc855bac87a55cc979a
Gauri Bhand <htcgauribhand@gmail.com>
2022-03-22 19:13:23 +0530

    32

--- Codechef/32_Second_max.cpp
+++ Codechef/32_Second_max.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 typedef long long ll;
 int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
-int t;cin>>t;
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
+int t;
+cin>>t;
 while(t--){
   int arr[3];
   for(int i = 0;i<3;i++){
