7c43fb5e4d81e01ce09caed5566ea2c0ca900a80
Gauri Bhand <htcgauribhand@gmail.com>
2022-03-25 08:35:31 +0530

    26

--- Codechef/26_uncle_johny.cpp
+++ Codechef/26_uncle_johny.cpp
@@ -3,13 +3,11 @@ using namespace std;
 int main(){
     int t;
     cin >> t;
-    for (int i = 0; i < t; i++)
-    {
+    for (int i = 0; i < t; i++) {
         int n, p, g, temp, coun = 0;
         cin >> n;
         int arr[n];
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++){
             cin >> arr[i];
         }
         cin >> p;
