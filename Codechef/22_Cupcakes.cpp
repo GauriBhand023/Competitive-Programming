af545b1f794c36b22dca2e66775b400aa583ca4c
Gauri Bhand <htcgauribhand@gmail.com>
2022-03-26 09:19:56 +0530

    22

--- Codechef/22_Cupcakes.cpp
+++ Codechef/22_Cupcakes.cpp
@@ -1,11 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
+int main(){
     int t;
     cin>>t;
-    while(t--)
-    {
+    while(t--){
         long int a;
         cin>>a;
         int ans = a/2+1;
