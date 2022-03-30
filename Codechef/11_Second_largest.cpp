d0052eb860b08187577f570a4b18e48de8a98dbf
Gauri Bhand <htcgauribhand@gmail.com>
2022-03-30 14:31:46 +0530

    11

--- Codechef/11_Second_largest.cpp
+++ Codechef/11_Second_largest.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int main ()
-{
+int main (){
 	int T,A,B,C,SL,UMax,UMin;
 	cin>>T;
-	for (int i=1;i<=T;i++)
-	{
+	for (int i=1;i<=T;i++){
 		UMax=0,UMin=0;
 		cin>>A>>B>>C;
 		UMax=max(max(A,B),C);
