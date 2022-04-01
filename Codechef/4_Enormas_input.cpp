c8d6754569f56f74cafa55c1f4becc0626196ccf
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-02 00:20:49 +0530

    4

--- Codechef/4_Enormas_input.cpp
+++ Codechef/4_Enormas_input.cpp
@@ -5,7 +5,6 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    
     int n, k;
     cin >> n >> k;
     int cnt=0;
@@ -17,8 +16,7 @@ int main(){
             {
                 cnt++;
             }
-        }
-    
+        } 
     cout << cnt << endl;
     return 0;
 }
