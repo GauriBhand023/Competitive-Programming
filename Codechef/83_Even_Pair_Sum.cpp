59bd34d73a920e6693c6586b518e98e518a4a54f
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-03 18:04:26 +0530

    83

---
+++ Codechef/83_Even_Pair_Sum.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll; 
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--){
+	    ll a,b;
+	    cin>>a>>b;
+	    ll even_a = a/2;
+	    ll even_b = b/2;
+	    ll odd_a = a-even_a;
+	    ll odd_b = b-even_b;
+	    cout<<(even_a*even_b)+(odd_a*odd_b)<<endl;
+	}
+	return 0;
+}
