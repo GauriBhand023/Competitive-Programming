e0b58f346a7f81086aec6495ea7cea6b352b6d59
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-11 20:48:21 +0530

    71

--- Codechef/71_Say_No_Drugs.cpp
+++ Codechef/71_Say_No_Drugs.cpp
@@ -2,18 +2,21 @@
 #define ll long long
 using namespace std;
 
-
 int main() {
 	ll int t,maxi=-1;
 	cin>>t;
 	ll int n,k,l,temp,nsp,a;
-	while(t--){
+	while(t--)
+	{
 	    cin>>n>>k>>l;
 	    ll int arr[n];
 	    for(ll int i=0;i<n;i++){
 	        cin>>arr[i];
 	    }
-	    if(n==1) {cout<<"Yes\n"; continue;}
+	    if(n==1)
+		 {
+			 cout<<"Yes\n"; continue;
+		}
 	    ll int maxi=*max_element(arr,arr+n-1);
 	    ll int myf=arr[n-1];
 	    a=maxi-myf;
