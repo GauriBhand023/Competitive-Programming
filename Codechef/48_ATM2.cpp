3a78efadc873164a64f918180ba4aeabd63abaf7
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-15 00:22:42 +0530

    48

--- Codechef/48_ATM2.cpp
+++ Codechef/48_ATM2.cpp
@@ -1,25 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
- int main() {
-  int t ;
-  int k , a;
-  cin>>t;
-  for( int i= 0; i< t; i++) {
-     cin>>a>>k;
-     int arr[a];
-     for( int i=0 ;i<a ; i++) {
-        cin>>arr[i];
-     }
-     for( int i =0; i<a ;i++){
-        if(arr[i]<k||arr[i]==k){
-            cout<<"1";
-            k=k-arr[i];
-        }
-        else{
-            cout<<"0";
-        }
-     }
-     cout<<endl;
-  }
-  return 0;
- }
+int main()
+{
+   int t;
+   int k, a;
+   cin >> t;
+   for (int i = 0; i < t; i++)
+   {
+      cin >> a >> k;
+      int arr[a];
+      for (int i = 0; i < a; i++)
+      {
+         cin >> arr[i];
+      }
+      for (int i = 0; i < a; i++)
+      {
+         if (arr[i] < k || arr[i] == k)
+         {
+            cout << "1";
+            k = k - arr[i];
+         }
+         else
+         {
+            cout << "0";
+         }
+      }
+      cout << endl;
+   }
+   return 0;
+}
