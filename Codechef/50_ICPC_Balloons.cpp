ca1f565dfad4c892de3932f9ff0306d04dbf2a67
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-15 00:21:34 +0530

    50

--- Codechef/50_ICPC_Balloons.cpp
+++ Codechef/50_ICPC_Balloons.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     int t;
     cin >> t;
-    while (t--){
+    while (t--)
+    {
         int n;
         cin >> n;
         int a[n];
@@ -11,7 +13,8 @@ int main(){
             cin >> a[i];
         int c = 0;
         int i;
-        for (i = 0; i < n; i++) {
+        for (i = 0; i < n; i++)
+        {
             if (a[i] >= 1 && a[i] <= 7)
                 c++;
             if (c >= 7)
