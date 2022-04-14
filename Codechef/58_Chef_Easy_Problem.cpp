39182ffdcdfe62f57a5603382582f32cecf14eb6
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-14 10:09:39 +0530

    58

--- Codechef/58_Chef_Easy_Problem.cpp
+++ Codechef/58_Chef_Easy_Problem.cpp
@@ -1,22 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-	int t=0;
-	cin>>t;
-	while(t--){
-	    int a;
-	    cin>>a;
-	    long A[a];
-	    for(int i=0;i<a;i++)
-	    cin>>A[i];
-	    
-	    sort(A,A+a);
-	    long count=0;
-	    for(int i=a-1;i>=0;i=i-2){
-	        if(i>=0)
-	        count+=A[i];
-	    }
-	    cout<<count<<endl; 
+int main()
+{
+	int t = 0;
+	cin >> t;
+	while (t--)
+	{
+		int a;
+		cin >> a;
+		long A[a];
+		for (int i = 0; i < a; i++)
+			cin >> A[i];
+
+		sort(A, A + a);
+		long count = 0;
+		for (int i = a - 1; i >= 0; i = i - 2)
+		{
+			if (i >= 0)
+				count += A[i];
+		}
+		cout << count << endl;
 	}
 	return 0;
 }
