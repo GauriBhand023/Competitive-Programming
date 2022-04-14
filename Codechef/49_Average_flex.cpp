429279c2d17c21d618032c2f7ec1c1527e5e69a3
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-15 00:22:09 +0530

    49

--- Codechef/49_Average_flex.cpp
+++ Codechef/49_Average_flex.cpp
@@ -1,26 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll; 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-int t;cin>>t;
-while(t--){
-  int a,b;
-  cin>>a;
-  vector<int> freq(101);
-  for(int i=0; i<a; i++){
-      cin>>b;
-      freq[b]++;
-  }
-  int ans=0, cnt=0;
-  for(int i=0; i<=100; i++){
-      if(freq[i]!=0){
-          cnt+=freq[i];
-          if(cnt>a-cnt)
-          ans+=freq[i];
-      }
-  }
-  cout<<ans<<endl;
-}
-return 0;
+typedef long long ll;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a;
+        vector<int> freq(101);
+        for (int i = 0; i < a; i++)
+        {
+            cin >> b;
+            freq[b]++;
+        }
+        int ans = 0, cnt = 0;
+        for (int i = 0; i <= 100; i++)
+        {
+            if (freq[i] != 0)
+            {
+                cnt += freq[i];
+                if (cnt > a - cnt)
+                    ans += freq[i];
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
 }
