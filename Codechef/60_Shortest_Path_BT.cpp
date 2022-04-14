c01e6f7110234b6a6fef38224502c5320724da9d
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-14 10:08:41 +0530

    60

--- Codechef/60_Shortest_Path_BT.cpp
+++ Codechef/60_Shortest_Path_BT.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 using namespace std;
 
-
-int main() {
+int main()
+{
 	int t;
-	cin>>t;
-	while(t--){
-	    int a,b;
-	    cin>>a>>b;
-	    int c=0;
-	    while(a!=b){
-	        c++;
-	        if(a>b){
-	            a/=2;     
-	        }
-	        else{
-	            b/=2;
-	        }
-	    }
-	    cout<<c<<endl;
+	cin >> t;
+	while (t--)
+	{
+		int a, b;
+		cin >> a >> b;
+		int c = 0;
+		while (a != b)
+		{
+			c++;
+			if (a > b)
+			{
+				a /= 2;
+			}
+			else
+			{
+				b /= 2;
+			}
+		}
+		cout << c << endl;
 	}
 	return 0;
 }
