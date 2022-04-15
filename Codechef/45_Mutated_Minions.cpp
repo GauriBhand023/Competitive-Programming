14a502554b958d9650307c9cbb2de33ca94640c0
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-16 01:40:31 +0530

    45

--- Codechef/45_Mutated_Minions.cpp
+++ Codechef/45_Mutated_Minions.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 using namespace std;
 #define mini_number d;
-int main() {
+int main()
+{
 	int t;
-	cin>>t;	
-	while(t--){
-	    int a,b,c,d=0;
-	     cin>>a>>b;
-	    for(int i=0;i<a;i++)
-	    {
-	        cin>>c;
-	        c=c+b;
-	         if(c%7==0)
-	            d++;       
-	    }    
-	    cout<<d<<endl;
+	cin >> t;
+	while (t--)
+	{
+		int a, b, c, d = 0;
+		cin >> a >> b;
+		for (int i = 0; i < a; i++)
+		{
+			cin >> c;
+			c = c + b;
+			if (c % 7 == 0)
+				d++;
+		}
+		cout << d << endl;
 	}
 	return 0;
 }
