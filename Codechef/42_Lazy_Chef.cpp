a8cf787151a0de864c936a99a0c926dbd0e88434
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-17 16:21:02 +0530

    42

--- Codechef/42_Lazy_Chef.cpp
+++ Codechef/42_Lazy_Chef.cpp
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
 	int t;
 	cin >> t;
-	while(t--){
-	    int a,b,c;
-	    cin >> a >> b >> c;
-	    cout << min(a+c,b*a) <<endl;
+	while (t--)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+		cout << min(a + c, b * a) << endl;
 	}
 	return 0;
 }
