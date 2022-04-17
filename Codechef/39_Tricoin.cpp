488de8c3c4e68b9621064031d4782fb6697ac93e
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-17 21:54:10 +0530

    39

--- Codechef/39_Tricoin.cpp
+++ Codechef/39_Tricoin.cpp
@@ -1,23 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
 	int t;
 	cin >> t;
-	while(t--){
-	    int n;
-	    cin >> n;
-	    int sum = 0,count = 0;
-	    for(int i = 1; i<=n; i++){
-	        sum +=i;
-	        count++;
-	        if(sum>n){
-	            cout << count-1 << endl;
-	            break;
-	        }else if(sum == n){
-	            cout << count << endl;
-	            break;
-	        }
-	    }
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		int sum = 0, count = 0;
+		for (int i = 1; i <= n; i++)
+		{
+			sum += i;
+			count++;
+			if (sum > n)
+			{
+				cout << count - 1 << endl;
+				break;
+			}
+			else if (sum == n)
+			{
+				cout << count << endl;
+				break;
+			}
+		}
 	}
 	return 0;
 }
