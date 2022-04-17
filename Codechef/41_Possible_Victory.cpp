74867e980c78945c534054f269c4aea00d33c100
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-17 16:21:33 +0530

    41

--- Codechef/41_Possible_Victory.cpp
+++ Codechef/41_Possible_Victory.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 using namespace std;
-int main() {
-	int a,b,c,d;
-	cin>>a>>b>>c;
-	d=(20-b)*36;
-	if(a<d+c) {
-                cout<<"YES"<<endl;
+int main()
+{
+        int a, b, c, d;
+        cin >> a >> b >> c;
+        d = (20 - b) * 36;
+        if (a < d + c)
+        {
+                cout << "YES" << endl;
         }
-        else{
-                cout<<"NO"<<endl;
+        else
+        {
+                cout << "NO" << endl;
         }
         return 0;
 }
