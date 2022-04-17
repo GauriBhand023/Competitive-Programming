1dfa6dcf5e4d35e909f9e33ee05337c0222ca23c
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-17 21:53:40 +0530

    40

--- Codechef/40_Chef_On_Island.cpp
+++ Codechef/40_Chef_On_Island.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 using namespace std;
-int main() {
+int main()
+{
 	int c;
-	cin>>c;
-	while(c--){
-	    float x,y,x1,y1,a,b;
-	    cin>>x>>y>>x1>>y1;
-	    int d;
-	    cin>>d; 
-	    if(min(x/x1,y/y1)>=d)
-        cout<<"YES"<<endl;
-	    else
-         cout<<"NO"<<endl;
+	cin >> c;
+	while (c--)
+	{
+		float x, y, x1, y1, a, b;
+		cin >> x >> y >> x1 >> y1;
+		int d;
+		cin >> d;
+		if (min(x / x1, y / y1) >= d)
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
 	}
 	return 0;
 }
