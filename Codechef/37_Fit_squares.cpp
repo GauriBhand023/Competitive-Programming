bfd4bd9a344973ed93bd1fcdb4e320ccb31ba0d6
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-18 02:34:18 +0530

    37

--- Codechef/37_Fit_squares.cpp
+++ Codechef/37_Fit_squares.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int t;cin>>t;
-    while(t--){
-        int n; cin>>n;
-        if(n%2==1){
-            n = n-1;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        if (n % 2 == 1)
+        {
+            n = n - 1;
         }
-        n/=2;
-        int area = n*n;
-        cout<<(area-n)/2<<endl;    
+        n /= 2;
+        int area = n * n;
+        cout << (area - n) / 2 << endl;
     }
-	return 0;
+    return 0;
 }
