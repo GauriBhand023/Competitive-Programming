0beb08bd2af899996165d6ad7fe78b89179c3331
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-17 21:54:50 +0530

    38

--- Codechef/38_Gross_Salary.cpp
+++ Codechef/38_Gross_Salary.cpp
@@ -1,22 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--)
+    {
         double basic_salary;
-        cin>>basic_salary;
-        double HRA=0,DA=0,gross_salary=0;
-        if(basic_salary<1500){
-            HRA=basic_salary*0.1;
-            DA=basic_salary*0.9;
+        cin >> basic_salary;
+        double HRA = 0, DA = 0, gross_salary = 0;
+        if (basic_salary < 1500)
+        {
+            HRA = basic_salary * 0.1;
+            DA = basic_salary * 0.9;
         }
-        else {
-            HRA=500;
-            DA=basic_salary*0.98;
+        else
+        {
+            HRA = 500;
+            DA = basic_salary * 0.98;
         }
-        gross_salary = basic_salary+HRA+DA;
-        cout<<fixed<<setprecision(2)<<gross_salary<<endl;
+        gross_salary = basic_salary + HRA + DA;
+        cout << fixed << setprecision(2) << gross_salary << endl;
     }
     return 0;
 }
