412505fc0f9fea922efaa75bff5c45e285362f4c
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-19 02:35:07 +0530

    81

--- Codechef/81_cup_finals.cpp
+++ Codechef/81_cup_finals.cpp
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+ {
   int t;
   cin>>t;
-  while(t--){
+  while(t--)
+  {
     int x,y,d;
     cin>>x>>y>>d;
     if(abs(x-y)<=d) 
