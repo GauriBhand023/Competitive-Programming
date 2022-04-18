503a8bd86b3b26b7bc9d25b1763d774bc097ea81
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-19 02:08:57 +0530

    31

--- Codechef/31_GCD_and_LCM.cpp
+++ Codechef/31_GCD_and_LCM.cpp
@@ -1,15 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-int main(){
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-cout.tie(NULL);
-int t;cin>>t;
-while(t--){
-  long long int x=0, y=0;
-  cin>>x>>y;
-  cout<<__gcd(x,y)<<" "<<((x*y)/__gcd(x,y))<<endl;
-}
-return 0;
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    long long int x = 0, y = 0;
+    cin >> x >> y;
+    cout << __gcd(x, y) << " " << ((x * y) / __gcd(x, y)) << endl;
+  }
+  return 0;
 }
