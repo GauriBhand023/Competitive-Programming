6bed3f06f5602f9eaa21c24093f83f24736a25c5
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-18 09:57:08 +0530

    33

--- Codechef/33_Greedy_puppy.cpp
+++ Codechef/33_Greedy_puppy.cpp
@@ -1,18 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-int main(){
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-cout.tie(NULL);
-int t;cin>>t;
-while(t--){
-  int n,k;cin>>n>>k;
-  int ans = n%k;
-  for(int i = 1 ;i<=k;i++){
-      if(ans<=n%i)ans=n%i;
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n, k;
+    cin >> n >> k;
+    int ans = n % k;
+    for (int i = 1; i <= k; i++)
+    {
+      if (ans <= n % i)
+        ans = n % i;
+    }
+    cout << ans << endl;
   }
-  cout<<ans<<endl;
-}
-return 0;
+  return 0;
 }
