c2077af17dbc948b64e8f9b22b2cbbeffb7c4ca2
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-21 00:53:00 +0530

    74

--- Codechef/74_Magical_Planks.cpp
+++ Codechef/74_Magical_Planks.cpp
@@ -1,21 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     int t;
-    cin>>t;
-    while(t--)  {
-        int n,i;
-        int count=1;
-        cin>>n;
+    cin >> t;
+    while (t--)
+    {
+        int n, i;
+        int count = 1;
+        cin >> n;
         string str;
-        cin>>str;   
-        for(i=1;i<n;i++) {
-            if(str[i]!=str[i-1]){
-                count+=1;
+        cin >> str;
+        for (i = 1; i < n; i++)
+        {
+            if (str[i] != str[i - 1])
+            {
+                count += 1;
             }
         }
-        cout<<(count)/2<<endl;
-    }  
-	return 0;
+        cout << (count) / 2 << endl;
+    }
+    return 0;
 }
