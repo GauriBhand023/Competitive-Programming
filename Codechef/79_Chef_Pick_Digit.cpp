81595215d87de8ec10e9ed83b634f90837859a7d
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-21 00:45:51 +0530

    79

--- Codechef/79_Chef_Pick_Digit.cpp
+++ Codechef/79_Chef_Pick_Digit.cpp
@@ -2,42 +2,51 @@
 #define int long long int
 using namespace std;
 
-int32_t main(){
+int32_t main()
+{
     std::ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
     int T;
     cin >> T;
-    while (T--){
+    while (T--)
+    {
         string str;
         cin >> str;
         map<int, int> mp;
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++)
+        {
             mp[str[i] - '0']++;
         }
         set<char> st;
-        for (int i = 6; i <= 9; i++){
+        for (int i = 6; i <= 9; i++)
+        {
             if (mp[i] == 0)
                 continue;
             mp[i]--;
-            for (int j = 0; j <= 9; j++){
-               int t = i * 10 + j;
-                if (mp[j] >= 1) {
-                    if (t >= 65 && t <= 90) {
+            for (int j = 0; j <= 9; j++)
+            {
+                int t = i * 10 + j;
+                if (mp[j] >= 1)
+                {
+                    if (t >= 65 && t <= 90)
+                    {
                         st.insert(t);
                     }
                 }
             }
             mp[i]++;
         }
-        if (st.size() == 0) {
+        if (st.size() == 0)
+        {
             cout << endl;
             continue;
         }
-        for (auto it : st){
+        for (auto it : st)
+        {
             cout << it;
         }
-       cout << endl;
+        cout << endl;
     }
     return 0;
 }
