592087ee967052cf94380f98c80dbe0e8d60f85f
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-21 00:50:49 +0530

    76

--- Codechef/76_Harrenhal.cpp
+++ Codechef/76_Harrenhal.cpp
@@ -1,18 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int is_pal(string s){
-    string s2=s;
+int is_pal(string s)
+{
+    string s2 = s;
     reverse(s2.begin(), s2.end());
-    if(s==s2) return 1;
-    else return 0;
+    if (s == s2)
+        return 1;
+    else
+        return 0;
 }
 
-int main(){
-    int t=1;
-    cin>>t;
-    while(t--){
-        string s; cin>>s;
+int main()
+{
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
         s[s.size()];
         cout << (is_pal(s) ? "1\n" : "2\n");
     }
