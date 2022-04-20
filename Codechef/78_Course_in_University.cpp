af61917ed21bf7efdf6254f19e221883ff85b939
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-21 00:49:40 +0530

    78

--- Codechef/78_Course_in_University.cpp
+++ Codechef/78_Course_in_University.cpp
@@ -1,21 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define z long long int
-void solve(){
+void solve()
+{
     z n;
-    cin>>n;
+    cin >> n;
     z arr[n];
-    for(int i=0;i<n;i++) {
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    sort(arr,arr+n);
-    cout<<(n-arr[n-1])<<endl;
+    sort(arr, arr + n);
+    cout << (n - arr[n - 1]) << endl;
 }
-int main(){
-	z t;
-	cin>>t;
-	while(t--){
-	    solve();
-	}
-	return 0;
+int main()
+{
+    z t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
 }
