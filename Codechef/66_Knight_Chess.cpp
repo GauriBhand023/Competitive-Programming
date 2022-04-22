46e0504152e7a559c053ab17827cdaab89c05f5a
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-22 13:33:42 +0530

    66

--- Codechef/66_Knight_Chess.cpp
+++ Codechef/66_Knight_Chess.cpp
@@ -2,54 +2,69 @@
 using namespace std;
 
 #define int long long
-signed main(){
+signed main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         int n;
         cin >> n;
         int a[n], b[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             cin >> a[i] >> b[i];
         }
         int x, y;
         cin >> x >> y;
         int check = 0;
-        for (int i = 0; i < n; i++) {
-            if (a[i] == (x - 2) && b[i] == (y - 1)) {
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] == (x - 2) && b[i] == (y - 1))
+            {
                 check = 1;
             }
-            if (a[i] == (x - 2) && b[i] == (y + 1)) {
+            if (a[i] == (x - 2) && b[i] == (y + 1))
+            {
                 check = 1;
             }
-            if (a[i] == (x + 2) && b[i] == (y - 1)) {
+            if (a[i] == (x + 2) && b[i] == (y - 1))
+            {
                 check = 1;
             }
-            if (a[i] == (x + 2) && b[i] == (y + 1)) {
+            if (a[i] == (x + 2) && b[i] == (y + 1))
+            {
                 check = 1;
             }
-            if (a[i] == (x - 1) && b[i] == (y - 2)) {
+            if (a[i] == (x - 1) && b[i] == (y - 2))
+            {
                 check = 1;
             }
-            if (a[i] == (x + 1) && b[i] == (y - 2)){
+            if (a[i] == (x + 1) && b[i] == (y - 2))
+            {
                 check = 1;
             }
-            if (a[i] == (x - 1) && b[i] == (y + 2)) {
+            if (a[i] == (x - 1) && b[i] == (y + 2))
+            {
                 check = 1;
             }
-            if (a[i] == (x + 1) && b[i] == (y + 2)){
+            if (a[i] == (x + 1) && b[i] == (y + 2))
+            {
                 check = 1;
             }
-            if (check == 1){
+            if (check == 1)
+            {
                 break;
             }
         }
-        if (check == 1) {
+        if (check == 1)
+        {
             cout << "YES" << endl;
         }
-        else {
+        else
+        {
             cout << "NO" << endl;
         }
     }
