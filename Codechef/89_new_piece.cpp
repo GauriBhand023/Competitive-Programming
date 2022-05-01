5e3fd71f15e0724b3e3d76e5efce4a1544b450c9
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-01 22:27:42 +0530

    89

---
+++ Codechef/89_new_piece.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	while(n--)
+	{
+	     int a,b,c,d;
+	     cin>>a>>b>>c>>d;
+	     if(a==c&&b==d)
+	     {
+	         cout<<"0";
+	     }
+	     else
+	     {
+	     if((a+c)%2==0&&(b+d)%2==0) cout<<"2";
+	     else if((a+c)%2!=0&&(b+d)%2!=0) cout<<"2";
+	     else if((a+c)%2!=0||(b+d)%2!=0) cout<<"1";
+	     }
+	     cout<<endl;
+	     
+	}
+	return 0;
+}
