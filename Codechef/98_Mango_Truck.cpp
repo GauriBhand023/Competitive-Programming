77580c9ee438537afb3e0ef06b9fe85d41bfbce8
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-05 00:16:34 +0530

    98

--- Codechef/98_Mango_Truck.cpp
+++ Codechef/98_Mango_Truck.cpp
@@ -3,20 +3,15 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
 
 int t;
 cin>>t;
 while(t--){
-     int a;
-     cin>>a;
-
-     int b;
-     cin>>b;
-
-     int c;
-     cin>>c;
-
+     int a,b,c;
+     cin>>a>>b>>c;
      if(((c-b)/a)<=c){
          cout<<((c-b)/a);
      }
