7ba23849274f15b2e87f83133687e98c6a05abef
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-06 00:21:29 +0530

    99

--- Codechef/99_Max_Min.cpp
+++ Codechef/99_Max_Min.cpp
@@ -4,7 +4,9 @@ typedef long long ll;
 
  
 int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
 
 int t;cin>>t;
 while(t--){
