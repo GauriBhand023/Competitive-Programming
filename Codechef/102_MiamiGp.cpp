46f21c9a05241f3f84e003547a325e185c88d259
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-06 22:01:07 +0530

    102

---
+++ Codechef/102_MiamiGp.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  int x,y;
+  cin>>x>>y;
+  double z = 1.07*x;
+  if(y<=z){
+      cout<<"YES"<<endl;
+  }
+  else{
+      cout<<"NO"<<endl;
+  }
+}
+return 0;
+}
