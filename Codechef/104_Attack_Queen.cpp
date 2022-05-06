6c5bba85d7dd689a6f272ae486dcd09d7cf3e41d
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-06 22:30:25 +0530

    104

---
+++ Codechef/104_Attack_Queen.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  int n,x,y,a,b,c,d,e;
+  cin>>n>>x>>y;
+
+  a = n-x+x-1+n-y+y-1;
+  
+  b = min(x-1,y-1);
+
+  c= min(x-1,n-y);
+
+  d = min(n-x, y-1);
+
+  e = min(n-x, n-y);
+
+  int total = a+b+c+d+e;
+  cout<<total<<endl;
+}
+return 0;
+}
