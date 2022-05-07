71235afec253df8c770edae05b3058e9a5713a09
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-07 14:36:00 +0530

    106

---
+++ Codechef/105_MysticalN0.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0; i<n; i++){
+      cout<<arr[i];
+  }
+  int dp[n+1][33];
+  for(int i=1; i<=n; i++){
+      int temp;
+      temp = arr[i-1];
+      for(int j=0; j<33; j++){
+          dp[i][j]=dp[i-1][j];
+      }
+      int pw = 32;
+      if(temp!=0){
+          pw = (int)(log(temp)/log(2));
+          dp[i][pw]++;
+      }
+  }
+}
+return 0;
+}
---
+++ Codechef/106_pushpa.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  int n;
+  cin>>n;
+  int arr[n],m=0;
+  map<int, int> map;
+  for(int i=0; i<n; i++){
+      cin>>arr[i];
+      m = max(m, arr[i]);
+      map[arr[i]]++;
+  }
+  for(int i =0; i<n; i++){
+      m = max(m,arr[i]+map[arr[i]]-1);
+  }
+  cout<<m<<endl;
+}
+return 0;
+}
