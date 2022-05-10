5473bca23c42cc07f059cd784c5a64d68fd7e5f6
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-10 08:28:45 +0530

    103

--- Codechef/103_SugarCane.cpp
+++ Codechef/103_SugarCane.cpp
@@ -3,7 +3,9 @@ using namespace std;
 typedef long long ll;
  
 int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
 
 int t;cin>>t;
 while(t--){
