04498aed143f67d1d2f0efcf00ad7cba680c6832
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-11 21:27:25 +0530

    111

---
+++ Codechef/111_get_subscription.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int y ;
+    cin>>y;
+    while(y--) {
+
+   
+    
+    int X;
+    cin>>X;
+
+    int H = 30;
+
+    if(X>H){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+     }
+
+
+
+     
+return 0;
+}
