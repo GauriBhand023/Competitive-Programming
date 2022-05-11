0c207b86d9399a1d1785cd7a390dde9a8a2e9cdd
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-11 21:26:19 +0530

    110

---
+++ Codechef/110_circular.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  int a,b,m;
+  cin>>a>>b>>m;
+  if(a>b){
+    cout<<min(a-b,m-a+b);
+  }
+  else{
+    cout<<min(b-a, a+m-b);
+  }
+  cout<<endl;
+}
+return 0;
+}
