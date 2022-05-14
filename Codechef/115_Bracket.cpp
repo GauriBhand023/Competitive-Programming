f9f09ba181c6e1fcf8d1506ab89fb5734d8f7cca
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-14 22:18:44 +0530

    115

---
+++ Codechef/115_Bracket.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t; cin>>t;
+	while(t--)
+	{
+		string s; 
+        cin>>s;
+		stack<char> st;
+		int mx=0;
+		for(int i=0; i<s.size(); i++)
+		{
+			if(s[i]=='(') st.push('(');
+			else st.pop();
+			if(st.size()>mx) mx=st.size();
+		}
+		for(int i=0; i<mx*2; i++)
+		{
+			if(i<mx) cout<<'(';
+			else cout<<')';
+		}
+		cout<<"\n";
+	}
+}
