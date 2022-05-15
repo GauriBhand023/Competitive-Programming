620327cf35627cbaf510d1798c1aa878ade30a28
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-15 20:39:23 +0530

    120

---
+++ Codechef/119_bucket.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  int a,b,c,d,e,f;
+  cin>>a>>b>>c>>d;
+  e=a+c*d;
+  f=max(a,max(b,max(c,d)));
+  if(e>f){
+      cout<<"overFlow"<<endl;
+  }
+  else if(e<f){
+      cout<<"unfilled"<<endl;
+  }
+  else{
+      cout<<"filled"<<endl;
+  }
+  
+}
+return 0;
+}
---
+++ Codechef/120_to_do.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int d[n];
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin>>d[i];
+            if(d[i]>=1000){
+                count++;
+            }
+        }
+        cout<<count<<endl;
+    }
+}
