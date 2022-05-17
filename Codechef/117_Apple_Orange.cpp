06f8d6eaaccddc45933897ce134f9b169703c37c
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-17 15:07:17 +0530

    117

--- Codechef/117_Apple_Orange.cpp
+++ Codechef/117_Apple_Orange.cpp
@@ -4,7 +4,8 @@ typedef long long ll;
 
  
 int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);cout.tie(NULL);
 
   int a,b,c;
   cin>>a>>b>>c;
