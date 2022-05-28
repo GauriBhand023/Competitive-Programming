fce32a2c9421d16b139fc1630439d34833718ca8
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-28 22:32:26 +0530

    126

--- Codechef/99_Max_Min.cpp
+++ Codechef/99_Max_Min.cpp
@@ -1,31 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-cout.tie(NULL);
-
-int t;cin>>t;
-while(t--){
-  int n,x;
-  cin>>n>>x;
-  int arr[n];
-  for(int i=0; i<n-1; i++){
-      cin>>arr[i];
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n, x;
+    cin >> n >> x;
+    int arr[n];
+    for (int i = 0; i < n - 1; i++)
+    {
+      cin >> arr[i];
+    }
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+      if (max < arr[i])
+        max = arr[i];
+    }
+    if (max < n)
+    {
+      cout << max << endl;
+    }
+    else
+      cout << n << endl;
   }
-  int max = arr[0];
-  for(int i=1;i<n; i++){
-      if(max<arr[i])
-      max=arr[i];
-  }
-  if(max<n){
-      cout<<max<<endl;
-  }
-  else
-    cout<<n<<endl;    
-  }
-return 0;
+  return 0;
 }
