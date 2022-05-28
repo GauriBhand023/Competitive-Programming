fc45dfe9c536a0977c1789f089f1dca86de79a9e
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-29 00:36:54 +0530

    125

--- Codechef/98_Mango_Truck.cpp
+++ Codechef/98_Mango_Truck.cpp
@@ -1,20 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
-int main(){
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-cout.tie(NULL);
+int main()
+{
 
-int t;
-cin>>t;
-while(t--){
-     int a,b,c;
-     cin>>a>>b>>c;
-     if(((c-b)/a)<=c){
-         cout<<((c-b)/a);
-     }
-}
-return 0;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (((c - b) / a) <= c)
+        {
+            cout << ((c - b) / a);
+        }
+    }
+    return 0;
 }
