4d4a36dac03e7ed3d4eb1989a7ec69296ed625d0
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-31 00:48:07 +0530

    127

--- Codechef/125_chessRating.cpp
+++ Codechef/125_chessRating.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
   int t;
-  cin>>t;
-  while(t--){
-      int a,b;
-      cin>>a>>b;
-      cout<<(b-a+7)/8<<'\n';
+  cin >> t;
+  while (t--)
+  {
+    int a, b;
+    cin >> a >> b;
+    cout << (b - a + 7) / 8 << '\n';
   }
   return 0;
 }
