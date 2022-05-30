fc4f366601c56464d047b09955dcd33b9d53b7c2
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-30 21:39:02 +0530

    126

---
+++ Codechef/127_Make_them_zero.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int t, n, x;
+	cin >> t;
+	while(t--){
+	    cin >> n;
+	    int osm = 0, ans = 0;
+	    for(int i=0; i<n; ++i){
+	        cin >> x;
+	        osm |= x;
+	    }   
+	    while(osm){
+	        ans += osm%2;
+	        osm /= 2;
+	    }
+	    cout << ans << "\n";
+	}
+	return 0;
+}
