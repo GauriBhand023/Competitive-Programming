d0fa1b4b649b51b9a89a69a773eb54252e05f0db
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-30 14:20:02 +0530

    126

--- Codechef/94_Subtask.cpp
+++ Codechef/94_Subtask.cpp
@@ -1,26 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-int t;
-cin>>t;
-while(t--){
-    int n,m,k;
-    cin>>n>>m>>k;
-    vector<int> v(n);
-    int sum=0;
-    int ans=0;
-    for(int i=0; i<n; i++){
-        cin>>v[i];
-        sum+=v[i];
-        if(i==m-1 and sum==m){
-            ans=k;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m, k;
+        cin >> n >> m >> k;
+        vector<int> v(n);
+        int sum = 0;
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+            sum += v[i];
+            if (i == m - 1 and sum == m)
+            {
+                ans = k;
+            }
         }
+        if (sum == n)
+        {
+            ans = 100;
+        }
+        cout << ans << endl;
     }
-    if(sum==n){
-        ans=100;
-    }
-    cout<<ans<<endl;
-}
-return 0;
+    return 0;
 }
