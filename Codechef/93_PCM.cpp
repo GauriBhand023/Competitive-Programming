4f6547774cb95e31424cbf09920c6cc89f5dcc45
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-30 14:20:33 +0530

    126

--- Codechef/93_PCM.cpp
+++ Codechef/93_PCM.cpp
@@ -1,19 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
 	int T;
-	cin>>T;
-	while(T--)
+	cin >> T;
+	while (T--)
 	{
-	    string S;
-        cin>>S;
-        sort(S.begin(),S.end());
-        
-        if (S=="CMP")
-            cout<<"YES"<<endl;
-        else 
-            cout<<"NO"<<endl;
+		string S;
+		cin >> S;
+		sort(S.begin(), S.end());
+
+		if (S == "CMP")
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
 	}
 	return 0;
 }
