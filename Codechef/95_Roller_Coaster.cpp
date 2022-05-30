9a6069d35189bd4490c4dbc7cba985e8a238ab21
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-30 14:19:32 +0530

    126

--- Codechef/95_Roller_Coaster.cpp
+++ Codechef/95_Roller_Coaster.cpp
@@ -1,22 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
 
-int t;
-cin>>t;
-while(t--){
-    int X,H;
-    cin>>X>>H;
-    if(X>=H){
-        cout<<"YES"<<endl;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int X, H;
+        cin >> X >> H;
+        if (X >= H)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
-    else{
-        cout<<"NO"<<endl;
-    }
-}
-return 0;
+    return 0;
 }
