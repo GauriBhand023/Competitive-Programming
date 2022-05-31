2af640c496fe10f46b0ccff9a917c7fab752cc42
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-31 22:13:47 +0530

    126

--- Codechef/124_1st_Contest.cpp
+++ Codechef/124_1st_Contest.cpp
@@ -1,16 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
-
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-  int a,b,c;
-  cin>>a>>b>>c;
-  int d=a-b;
-  cout<<d<<endl;
-  int e=d-c;
-  cout<<e<<endl;
+int main()
+{
+  int a, b, c;
+  cin >> a >> b >> c;
+  int d = a - b;
+  cout << d << endl;
+  int e = d - c;
+  cout << e << endl;
   return 0;
 }
-
