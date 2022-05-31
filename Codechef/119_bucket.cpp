58698b3cec153a0cdfca147ae40a6bb1f0a7543c
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-31 22:16:04 +0530

    126

--- Codechef/119_bucket.cpp
+++ Codechef/119_bucket.cpp
@@ -1,25 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-int t;
-cin>>t;
-while(t--){
-  int a,b,c,d,e,f;
-  cin>>a>>b>>c>>d;
-  e=a+c*d;
-  f=max(a,max(b,max(c,d)));
-  if(e>f){
-      cout<<"overFlow"<<endl;
-  }
-  else if(e<f){
-      cout<<"unfilled"<<endl;
-  }
-  else{
-      cout<<"filled"<<endl;
-  }
-}
-return 0;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c, d, e, f;
+        cin >> a >> b >> c >> d;
+        e = a + c * d;
+        f = max(a, max(b, max(c, d)));
+        if (e > f)
+        {
+            cout << "overFlow" << endl;
+        }
+        else if (e < f)
+        {
+            cout << "unfilled" << endl;
+        }
+        else
+        {
+            cout << "filled" << endl;
+        }
+    }
+    return 0;
 }
