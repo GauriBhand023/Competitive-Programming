3b8b4ce8948585a7e2a0cf4801fa5fa2542d309e
Gauri Bhand <htcgauribhand@gmail.com>
2022-05-31 22:17:07 +0530

    125

--- Codechef/117_Apple_Orange.cpp
+++ Codechef/117_Apple_Orange.cpp
@@ -1,19 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
-int main(){
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);cout.tie(NULL);
-
-  int a,b,c;
-  cin>>a>>b>>c;
-  if(a<b+c){
-      cout<<"no"<<endl;
+int main()
+{
+  int a, b, c;
+  cin >> a >> b >> c;
+  if (a < b + c)
+  {
+    cout << "no" << endl;
   }
-  else{
-      cout<<"yes"<<endl;
+  else
+  {
+    cout << "yes" << endl;
   }
   return 0;
 }
-
