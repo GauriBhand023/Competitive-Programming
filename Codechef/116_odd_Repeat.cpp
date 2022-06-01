630e4e74064f60d235be0b49a7492d006b51b37b
Gauri Bhand <htcgauribhand@gmail.com>
2022-06-01 11:10:25 +0530

    126

--- Codechef/116_odd_Repeat.cpp
+++ Codechef/116_odd_Repeat.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
-#include<stdlib.h>
+#include <stdlib.h>
 using namespace std;
 
-int main() {
-    long int t,n,s,k,i;
-    cin>>t;
-    while(t--)
+int main()
+{
+    long int t, n, s, k, i;
+    cin >> t;
+    while (t--)
     {
-        cin>>n>>k>>s;
-        int sum=n*n;
-        sum=s-sum;
+        cin >> n >> k >> s;
+        int sum = n * n;
+        sum = s - sum;
         k--;
-        long int ans=sum/k;
-        cout<<ans<<endl;      
+        long int ans = sum / k;
+        cout << ans << endl;
     }
-	return 0;
+    return 0;
 }
