baf9c36f002ab56918514c4510c6a2bfc95b8e44
Gauri Bhand <htcgauribhand@gmail.com>
2022-06-02 11:45:48 +0530

    132

--- Codechef/132_path_parity.cpp
+++ Codechef/132_path_parity.cpp
@@ -1,24 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
-int t;cin>>t;
-while(t--){
-  int x,y;
-  cin>>x>>y;
-  if(y==1){
-      cout<<"yes"<<endl;
-  }
-  else if (x%2==0){
-      cout<<"yes"<<endl;
-  }
-  else{
-      cout<<"no"<<endl;
-  }
-}
-return 0;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        if (y == 1)
+        {
+            cout << "yes" << endl;
+        }
+        else if (x % 2 == 0)
+        {
+            cout << "yes" << endl;
+        }
+        else
+        {
+            cout << "no" << endl;
+        }
+    }
+    return 0;
 }
