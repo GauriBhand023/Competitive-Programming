06307cfaecaf485c87f8ff0381b931951a203915
Gauri Bhand <htcgauribhand@gmail.com>
2022-06-02 11:48:25 +0530

    132

--- Codechef/130_equal_int.cpp
+++ Codechef/130_equal_int.cpp
@@ -1,35 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
-int t;cin>>t;
-while(t--){
-  int x,y,cnt=0;
-  cin>>x>>y;
-  while(x!=y){
-      if(x<y){
-          int t = y-x;
-          cnt+=t;
-          x+=t;
-      }
-      if(y<x){
-          if(x-y==1){
-              y+=2;
-              cnt++;
-          }
-          else{
-              int t = (x-y)/2;
-              cnt+=t;
-              t*=2;
-              y+=t;
-          }
-      }
-  }
-  cout<<cnt<<endl;
-}
-return 0;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y, cnt = 0;
+        cin >> x >> y;
+        while (x != y)
+        {
+            if (x < y)
+            {
+                int t = y - x;
+                cnt += t;
+                x += t;
+            }
+            if (y < x)
+            {
+                if (x - y == 1)
+                {
+                    y += 2;
+                    cnt++;
+                }
+                else
+                {
+                    int t = (x - y) / 2;
+                    cnt += t;
+                    t *= 2;
+                    y += t;
+                }
+            }
+        }
+        cout << cnt << endl;
+    }
+    return 0;
 }
