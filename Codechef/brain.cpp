a23aa8d42810fba287390036b3e69aec9492b559
Gauri Bhand <htcgauribhand@gmail.com>
2022-06-03 10:22:27 +0530

    132

--- Codechef/brain.cpp
+++ Codechef/brain.cpp
@@ -1,17 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-    int a,b;
-    cin>>a>>b;
-    if(a!=b){
-        cout<<"YES";
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a != b)
+    {
+        cout << "YES";
     }
-    else{
-        cout<<"NO";
+    else
+    {
+        cout << "NO";
     }
 
-return 0;
+    return 0;
 }
