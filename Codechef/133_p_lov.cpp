b8db6f6e7cbae1bee5351c3662cfd0998b3be073
Gauri Bhand <htcgauribhand@gmail.com>
2022-06-07 12:12:19 +0530

    140

--- Codechef/133_p_lov.cpp
+++ Codechef/133_p_lov.cpp
@@ -1,22 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
-int t;cin>>t;
-while(t--){
-        int a,b,c;
-        cin>>a>>b>>c;
-        if(c*b>=a){
-            cout<<"YES"<<endl;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (c * b >= a)
+        {
+            cout << "YES" << endl;
         }
-        else{
-            cout<<"NO"<<endl;
+        else
+        {
+            cout << "NO" << endl;
         }
     }
     return 0;
 }
-
