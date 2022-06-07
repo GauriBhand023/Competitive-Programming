888f398e92fac426b9c85f47fa739697921b15be
Gauri Bhand <htcgauribhand@gmail.com>
2022-06-07 12:13:20 +0530

    140

--- Codechef/131_priority.cpp
+++ Codechef/131_priority.cpp
@@ -1,31 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
-int t;cin>>t;
-while(t--){
-  int a,b;
-  cin>>a>>b;
-  if(a==1){
-      if(b%2){
-          cout<<"ODD"<<endl;
-      }
-      else{
-          cout<<"EVEN"<<endl;
-      }
-  }
-  else{
-      if(a==2){
-          cout<<"ODD"<<endl;
-      }
-      else{
-          cout<<"EVEN"<<endl;
-      }
-  }
-}
-return 0;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (a == 1)
+        {
+            if (b % 2)
+            {
+                cout << "ODD" << endl;
+            }
+            else
+            {
+                cout << "EVEN" << endl;
+            }
+        }
+        else
+        {
+            if (a == 2)
+            {
+                cout << "ODD" << endl;
+            }
+            else
+            {
+                cout << "EVEN" << endl;
+            }
+        }
+    }
+    return 0;
 }
