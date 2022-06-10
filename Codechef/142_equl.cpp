863fc54603634eb4de4d06994a36284d50969f6e
Gauri Bhand <htcgauribhand@gmail.com>
2022-06-10 23:23:19 +0530

    145

--- Codechef/142_equl.cpp
+++ Codechef/142_equl.cpp
@@ -2,11 +2,9 @@
 using namespace std;
 typedef long long ll;
 
- 
 int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
-int t;cin>>t;
+int t;
+cin>>t;
 while(t--){
   int a,b,c;
   cin>>a>>b;
