5004445b0d9ab10879e96cf55932c580dc877e6b
Gauri Bhand <htcgauribhand@gmail.com>
2022-06-11 18:42:34 +0530

    145

--- Codechef/142_equl.cpp
+++ Codechef/142_equl.cpp
@@ -1,20 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
-int main(){
-int t;
-cin>>t;
-while(t--){
-  int a,b,c;
-  cin>>a>>b;
-  c=a+b;
-  if(c%2==0){
-      cout<<"YES"<<endl;
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int a, b, c;
+    cin >> a >> b;
+    c = a + b;
+    if (c % 2 == 0)
+    {
+      cout << "YES" << endl;
+    }
+    else
+    {
+      cout << "NO" << endl;
+    }
   }
-  else{
-      cout<<"NO"<<endl;
-  }
-}
-return 0;
+  return 0;
 }
