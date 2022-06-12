0aa448b4d15fbcccd9a771c8c8b6f2ea926068f0
Gauri Bhand <htcgauribhand@gmail.com>
2022-06-12 10:48:28 +0530

    146

--- Codechef/141_AgeLimit.cpp
+++ Codechef/141_AgeLimit.cpp
@@ -1,19 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
-int main(){
-int t;
-cin>>t;
-while(t--){
-  int a,b,c;
-  cin>>a>>b>>c;
-  if(c>=a && c<b){
-      cout<<"YES"<<endl;
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (c >= a && c < b)
+    {
+      cout << "YES" << endl;
+    }
+    else
+    {
+      cout << "NO" << endl;
+    }
   }
-  else{
-      cout<<"NO"<<endl;
-  }
-}
-return 0;
+  return 0;
 }
