4da4ced60fe2bc2d88ada17059748f5c56771ae1
Gauri Bhand <htcgauribhand@gmail.com>
2022-06-13 10:31:28 +0530

    146

--- Codechef/111_FootBall_cup.cpp
+++ Codechef/111_FootBall_cup.cpp
@@ -1,26 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
-int t;
-cin>>t;
-while(t--){
-  int x,y;
-  cin>>x>>y;
-if(x>0 || y>0){
-  if(x==y){
-      cout<<"YES"<<endl;
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int x, y;
+    cin >> x >> y;
+    if (x > 0 || y > 0)
+    {
+      if (x == y)
+      {
+        cout << "YES" << endl;
+      }
+      else
+      {
+        cout << "NO" << endl;
+      }
+    }
+    else
+    {
+      cout << "NO" << endl;
+    }
   }
-  else{
-      cout<<"NO"<<endl;
-  }
-}
-else{
-    cout<<"NO"<<endl;
-}
-}
-return 0;
+  return 0;
 }
