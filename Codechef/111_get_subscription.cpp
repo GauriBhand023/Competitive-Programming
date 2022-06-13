06af0949bd5dfc8256e68670aec3b47abcba6492
Gauri Bhand <htcgauribhand@gmail.com>
2022-06-13 10:29:49 +0530

    146

--- Codechef/111_get_subscription.cpp
+++ Codechef/111_get_subscription.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 int main()
 {
-    int y ;
-    cin>>y;
-    while(y--) {
-    int X;
-    cin>>X;
-    int H = 30;
-    if(X>H){
-        cout<<"YES"<<endl;
+    int y;
+    cin >> y;
+    while (y--)
+    {
+        int X;
+        cin >> X;
+        int H = 30;
+        if (X > H)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
-    else{
-        cout<<"NO"<<endl;
-    }
-     }
 
-
-
-     
-return 0;
+    return 0;
 }
