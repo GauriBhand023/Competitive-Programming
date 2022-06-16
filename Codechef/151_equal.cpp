cd9192a15ec58d018a08bb0761e9799fd68b4d7d
Gauri Bhand <htcgauribhand@gmail.com>
2022-06-16 19:05:10 +0530

    156

--- Codechef/151_equal.cpp
+++ Codechef/151_equal.cpp
@@ -1,31 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-
-
-int t;cin>>t;
-while(t--){
-  int c =0;
-  int a,b;
-  cin>>a>>b;
-  if(a==b){
-    cout<<"0"<<endl;
-  }
-  else if(b>a){
-    cout<<b-a<<endl;
-  }
-  else{
-    if(b%2 ==0){
-        cout<<(a-b)/2<<endl;
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int c = 0;
+    int a, b;
+    cin >> a >> b;
+    if (a == b)
+    {
+      cout << "0" << endl;
     }
-    else{
-        cout<<(((a+1-b)/2)+1)<<endl;
+    else if (b > a)
+    {
+      cout << b - a << endl;
+    }
+    else
+    {
+      if (b % 2 == 0)
+      {
+        cout << (a - b) / 2 << endl;
+      }
+      else
+      {
+        cout << (((a + 1 - b) / 2) + 1) << endl;
+      }
     }
   }
-}
 
-return 0;
+  return 0;
 }
