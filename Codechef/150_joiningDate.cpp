1e1338c90b9495e125861e3bf2890c967f3dccfb
Gauri Bhand <htcgauribhand@gmail.com>
2022-06-17 10:20:51 +0530

    156

--- Codechef/150_joiningDate.cpp
+++ Codechef/150_joiningDate.cpp
@@ -1,22 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-int t;cin>>t;
-while(t--){
-  int a,b;
-  cin>>a>>b;
-  int r = a/5;
-  if(a%5==0){
-    r--;
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int a, b;
+    cin >> a >> b;
+    int r = a / 5;
+    if (a % 5 == 0)
+    {
+      r--;
+    }
+    int f = b / 5;
+    if (b % 5 == 0)
+    {
+      f--;
+    }
+    cout << r - f << endl;
   }
-  int f = b/5;
-  if(b%5==0){
-    f--;
-  }
-  cout<<r-f<<endl;
-}
-return 0;
+  return 0;
 }
