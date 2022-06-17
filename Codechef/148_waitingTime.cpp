8317496a8ec43d9539063e7675f23f48afbaf69a
Gauri Bhand <htcgauribhand@gmail.com>
2022-06-17 10:22:08 +0530

    156

--- Codechef/148_waitingTime.cpp
+++ Codechef/148_waitingTime.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int t;
-    cin>>t;
-    while(t--){
-        int k,x;
-        cin>>k>>x;
-        cout<<(k*7)-x<<endl;
+    cin >> t;
+    while (t--)
+    {
+        int k, x;
+        cin >> k >> x;
+        cout << (k * 7) - x << endl;
     }
     return 0;
 }
