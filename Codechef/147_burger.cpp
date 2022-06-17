d9792b69b4c7935792681d69dc29ad7fed30c093
Gauri Bhand <htcgauribhand@gmail.com>
2022-06-17 10:23:20 +0530

    156

--- Codechef/147_burger.cpp
+++ Codechef/147_burger.cpp
@@ -1,19 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-int t;cin>>t;
-while(t--){
-  int a,b;
-  cin>>a>>b;
-  if(a<b){
-    cout<<a;
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int a, b;
+    cin >> a >> b;
+    if (a < b)
+    {
+      cout << a;
+    }
+    else
+    {
+      cout << b;
+    }
   }
-  else{
-    cout<<b;
-  }
-}
-return 0;
+  return 0;
 }
