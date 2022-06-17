b9a5a144c026a0245b2f6dc0f3ca1de7a981befb
Gauri Bhand <htcgauribhand@gmail.com>
2022-06-17 10:21:30 +0530

    156

--- Codechef/149_DNA.cpp
+++ Codechef/149_DNA.cpp
@@ -3,31 +3,31 @@ using namespace std;
 
 void D(char b, char c)
 {
-    if(b=='0' && c=='0')
-      cout<<"A";
-    else if(b=='0' && c=='1')
-      cout<<"T";
-    else if(b=='1' && c=='0')
-      cout<<"C"; 
-    else if(b=='1' && c=='1')
-      cout<<"G";   
+  if (b == '0' && c == '0')
+    cout << "A";
+  else if (b == '0' && c == '1')
+    cout << "T";
+  else if (b == '1' && c == '0')
+    cout << "C";
+  else if (b == '1' && c == '1')
+    cout << "G";
 }
 
 int main()
 {
-    int t;
-    cin>>t;
-    while(t--)
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n;
+    cin >> n;
+    string a;
+    cin >> a;
+    for (int j = 0; j < n; j += 2)
     {
-        int n;
-        cin>>n;
-        string a;
-        cin>>a;
-        for(int j=0;j<n;j+=2)
-        {
-            D(a[j],a[j+1]);
-        }
-        cout<<"\n";
+      D(a[j], a[j + 1]);
     }
-    return 0;
+    cout << "\n";
+  }
+  return 0;
 }
