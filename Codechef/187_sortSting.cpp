7b89e2f0585781b3ef885690da7aa007229c3602
Gauri Bhand <htcgauribhand@gmail.com>
2022-07-07 10:28:31 +0530

    187

--- Codechef/187_sortSting.cpp
+++ Codechef/187_sortSting.cpp
@@ -1,25 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-int t;cin>>t;
-while(t--){
-    long long n;
-    cin>>n;
-    string s;
-    cin>>s;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+        string s;
+        cin >> s;
 
-    int ans =0 ;
-    for(int i=0;i<n;i++){
-        if(s[i]=='1'&& s[i+1]=='0'){
-            ans+=1;
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '1' && s[i + 1] == '0')
+            {
+                ans += 1;
+            }
         }
+        cout << ans << endl;
     }
-    cout<<ans<<endl;
-}
-return 0;
+    return 0;
 }
