93a9cdbfe66e88f5c1a1ecf9eea3af033860cb66
Gauri Bhand <htcgauribhand@gmail.com>
2022-07-07 10:29:02 +0530

    187

--- Codechef/180_grpAssignment.cpp
+++ Codechef/180_grpAssignment.cpp
@@ -1,34 +1,42 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
-int ass(int n){
+int ass(int n)
+{
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
     map<int, int> mp;
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
         mp[arr[i]]++;
     }
     string ans = "YES";
-    for(auto it:mp){
-        if(it.second%it.first!=0){
-            ans= "NO";
+    for (auto it : mp)
+    {
+        if (it.second % it.first != 0)
+        {
+            ans = "NO";
             break;
         }
     }
-    cout<<ans<<endl;
+    cout << ans << endl;
 }
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-int t;cin>>t;
-while(t--){
-    int a;
-    ass(a);
-}
-return 0;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a;
+        ass(a);
+    }
+    return 0;
 }
