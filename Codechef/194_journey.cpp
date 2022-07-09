2c86626a969897e2fe3db546138782ee18990764
Gauri Bhand <htcgauribhand@gmail.com>
2022-07-09 13:41:55 +0530

    195

---
+++ Codechef/194_journey.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  int a,b,c,d;
+  cin>>a>>b>>c>>d;
+  int e = (a+b)&1;
+  int f = (c+d)&1;
+  if(e==f){
+    cout<<"YES"<<endl;
+  }
+  else{
+    cout<<"NO"<<endl;
+  }
+}
+return 0;
+}
