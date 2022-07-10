2e9d001f4949c768ef0504a5e23beb3596cf20fe
Gauri Bhand <htcgauribhand@gmail.com>
2022-07-10 17:02:40 +0530

    195

--- Codechef/191_Budding.cpp
+++ Codechef/191_Budding.cpp
@@ -1,24 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-int t;cin>>t;
-while(t--){
-  int a,b,c;
-  cin>>a>>b>>c;
-  if(c>a && c>b){
-    cout<<"Charlie"<<endl;
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (c > a && c > b)
+    {
+      cout << "Charlie" << endl;
+    }
+    else if (b > a && b > c)
+    {
+      cout << "Bob" << endl;
+    }
+    else if (a > b && a > c)
+    {
+      cout << "Alice" << endl;
+    }
   }
-  else if(b>a && b>c){
-    cout<<"Bob"<<endl;
-  }
-  else if(a>b && a>c){
-    cout<<"Alice"<<endl;
-  }
-}
-return 0;
+  return 0;
 }
