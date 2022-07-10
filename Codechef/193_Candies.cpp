b92dba0e0471b72feb7daa954c1d744653fc57b7
Gauri Bhand <htcgauribhand@gmail.com>
2022-07-10 17:03:38 +0530

    197

--- Codechef/193_Candies.cpp
+++ Codechef/193_Candies.cpp
@@ -1,28 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-int t;cin>>t;
-while(t--){
-  int a,b;
-  cin>>a>>b;
-  int c = a-b;
-  int buy = c/4;
-  if(b>a){
-    cout<<"0"<<endl;
-  }
-  else{
-    if(c%4==0){
-        cout<<buy<<endl;
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int a, b;
+    cin >> a >> b;
+    int c = a - b;
+    int buy = c / 4;
+    if (b > a)
+    {
+      cout << "0" << endl;
     }
-    else{
-        cout<<buy-1<<endl;
+    else
+    {
+      if (c % 4 == 0)
+      {
+        cout << buy << endl;
+      }
+      else
+      {
+        cout << buy - 1 << endl;
+      }
     }
   }
-}
-return 0;
+  return 0;
 }
