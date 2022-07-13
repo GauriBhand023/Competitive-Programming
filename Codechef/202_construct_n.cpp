e1ebdccecdd208a7293e1905a8869b86261281e2
Gauri Bhand <htcgauribhand@gmail.com>
2022-07-13 21:57:12 +0530

    200

--- Codechef/202_construct_n.cpp
+++ Codechef/202_construct_n.cpp
@@ -2,25 +2,24 @@
 using namespace std;
 typedef long long ll;
 
-long long sum(long long n, long long d){
-    n /= d;
-    return (n) * (1 + n) * d / 2;
-}
+int solve(int n){
+    cin>>n;
+    if(n<7 && n%2!=0){
+        cout<<"NO"<<endl;
+    }
+    else{
+        cout<<"YES"<<endl;
+    }
+ }
 
-long long sumMultiples(long long n)
-{
-    return sum(n, 2) + sum(n, 7);
-}
 
- 
 int main(){
 ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
 int t;cin>>t;
 while(t--){
-    long long n;
-    cin>>n;
-    cout << sumMultiples(n); 
+    int a;
+    solve(a);
 }
 return 0;
 }
