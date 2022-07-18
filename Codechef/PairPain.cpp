e55fbd2249d914ba7fff18524ceb6ac78cea0422
Gauri Bhand <htcgauribhand@gmail.com>
2022-07-18 10:26:18 +0530

    203

--- Codechef/PairPain.cpp
+++ Codechef/PairPain.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 	int t;
-	cin>>t;
-	for(int i=0;i<t;i++)
+	cin >> t;
+	for (int i = 0; i < t; i++)
 	{
-	    long long int d,c,c1=0,c2=0;
-	    cin>>d;
-	    int a[d];
-	    for(int j=0;j<d;j++)
-	    {
-	        cin>>a[j];
-	        if(a[j]==1)
-	        {
-	            c1++;
-	        }
-	        else if(a[j]==2)
-	        {
-	            c2++;
-	        }
-	    }
-	    c= c1*(d-c1)+c1*(c1-1)/2+c2*(c2-1)/2;
-	    cout<<c<<endl;
+		long long int d, c, c1 = 0, c2 = 0;
+		cin >> d;
+		int a[d];
+		for (int j = 0; j < d; j++)
+		{
+			cin >> a[j];
+			if (a[j] == 1)
+			{
+				c1++;
+			}
+			else if (a[j] == 2)
+			{
+				c2++;
+			}
+		}
+		c = c1 * (d - c1) + c1 * (c1 - 1) / 2 + c2 * (c2 - 1) / 2;
+		cout << c << endl;
 	}
 	return 0;
 }
