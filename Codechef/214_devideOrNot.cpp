743cfdc8195b154cadd35ee44c342028ea69a162
Gauri Bhand <htcgauribhand@gmail.com>
2022-07-23 18:18:52 +0530

    220

--- Codechef/214_devideOrNot.cpp
+++ Codechef/214_devideOrNot.cpp
@@ -1,27 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-int t;cin>>t;
-while(t--){
-  int x;
-  int a,b,n;
-  cin>>a>>b>>n;
-  if(a%b==0){
-    cout<<"-1"<<endl;
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int x;
+    int a, b, n;
+    cin >> a >> b >> n;
+    if (a % b == 0)
+    {
+      cout << "-1" << endl;
+    }
+    else
+    {
+      x = n;
+      if (x % a != 0)
+      {
+        x = n + a - (n % a);
+      }
+      for (x; !(x % a == 0 && x % b != 0); x = x + a)
+        ;
+      cout << x << endl;
+    }
   }
-  else{
-  x=n;
-  if(x%a!=0){
-    x=n+a - (n%a);
-  }
-  for(x; !(x%a ==0 && x%b!=0);x=x+a);
-    cout<<x<<endl;
-  } 
-}
-return 0;
+  return 0;
 }
