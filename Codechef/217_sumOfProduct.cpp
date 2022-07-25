539a6cfdb459fd2b4268bb154e3fa19c94f01977
Gauri Bhand <htcgauribhand@gmail.com>
2022-07-25 17:29:42 +0530

    220

--- Codechef/217_sumOfProduct.cpp
+++ Codechef/217_sumOfProduct.cpp
@@ -1,31 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-int t;cin>>t;
-while(t--){
-  int n;
-  cin>>n;
-  int c=0;
-  int ans=0;
-  for(int i=0; i<n; i++){
-    int temp;
-    cin>>temp;
-    if(temp==1){
-      c++;
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n;
+    cin >> n;
+    int c = 0;
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+      int temp;
+      cin >> temp;
+      if (temp == 1)
+      {
+        c++;
+      }
+      else
+      {
+        ans += (c * (c + 1) / 2);
+        c = 0;
+      }
     }
-    else{
-      ans+=(c*(c+1)/2);
-      c=0;
-    }
+    ans += (c * (c + 1) / 2);
+    cout << ans << endl;
   }
-  ans+=(c*(c+1)/2);
-  cout<<ans<<endl;
-   
-}
-return 0;
+  return 0;
 }
