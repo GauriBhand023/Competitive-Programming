c201f5724113ec1881ac6d5d89a30329589c7034
Gauri Bhand <htcgauribhand@gmail.com>
2022-07-30 22:37:56 +0530

    220

--- Codechef/212_chef.cpp
+++ Codechef/212_chef.cpp
@@ -1,16 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-int t;cin>>t;
-while(t--){
-  int a,b;
-  cin>>a>>b;
-  cout<<a*4+b<<endl;
-}
-return 0;
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int a, b;
+    cin >> a >> b;
+    cout << a * 4 + b << endl;
+  }
+  return 0;
 }
