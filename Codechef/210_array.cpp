f9f5d5293ea9f302cb9ea316b6ba93a80295a6d4
Gauri Bhand <htcgauribhand@gmail.com>
2022-08-03 17:52:37 +0530

    220

--- Codechef/210_array.cpp
+++ Codechef/210_array.cpp
@@ -1,27 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-int t;cin>>t;
-while(t--){
-  int a;
-  string s ;
-  cin>>a>>s;
-  int b = count(s.begin(), s.end(),'0');
-  int c = count(s.begin(), s.end(),'1');
-  if(b<c){
-    string f1(c,'1');
-    cout<<f1<<endl;
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int a;
+    string s;
+    cin >> a >> s;
+    int b = count(s.begin(), s.end(), '0');
+    int c = count(s.begin(), s.end(), '1');
+    if (b < c)
+    {
+      string f1(c, '1');
+      cout << f1 << endl;
+    }
+    else
+    {
+      string f1(b, '0');
+      cout << f1 << endl;
+    }
   }
-  else{2
-    string f1(b,'0');
-    cout<<f1<<endl;
-  }
-
-}
-return 0;
+  return 0;
 }
