cda8c87086e4b2d227f8c55b5062ac13418c30fe
Gauri Bhand <htcgauribhand@gmail.com>
2022-08-13 19:14:52 +0530

    240

--- Codechef/239_Two_Trains.cpp
+++ Codechef/239_Two_Trains.cpp
@@ -1,23 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-int t;cin>>t;
-while(t--){
-    int a,y,n,x;
-    cin>>n;
-    y=0;
-    a=0;
-    for(int i=1; i<n; ++i){
-        cin>>x;
-        a+=x;
-        y=max(y,x);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, y, n, x;
+        cin >> n;
+        y = 0;
+        a = 0;
+        for (int i = 1; i < n; ++i)
+        {
+            cin >> x;
+            a += x;
+            y = max(y, x);
+        }
+        cout << a + y << endl;
     }
-    cout<<a+y<<endl;
-}
-return 0;
+    return 0;
 }
