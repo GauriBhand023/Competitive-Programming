0ab09d4620e944f934f82c49b977d9e8e02dde94
Gauri Bhand <htcgauribhand@gmail.com>
2022-08-13 19:13:38 +0530

    240

--- Codechef/240_helloEducation.cpp
+++ Codechef/240_helloEducation.cpp
@@ -1,25 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
-void solve(){
+void solve()
+{
     int a;
-    cin>>a;
-    for(int i=1; i*i<=a; ++i){
-        if((a-2*i)%(i+2)==0 && a!=2*i){
-            cout<<"YES"<<endl;
+    cin >> a;
+    for (int i = 1; i * i <= a; ++i)
+    {
+        if ((a - 2 * i) % (i + 2) == 0 && a != 2 * i)
+        {
+            cout << "YES" << endl;
             return;
         }
     }
-    cout<<"NO"<<endl; 
+    cout << "NO" << endl;
 }
 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-int t;cin>>t;
-while(t--){
-    solve();
-}
-return 0;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
 }
