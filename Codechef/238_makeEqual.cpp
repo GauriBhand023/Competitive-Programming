f8b0ea3f839ea61719b2b2e3d3797c33325dacde
Gauri Bhand <htcgauribhand@gmail.com>
2022-08-13 19:15:56 +0530

    240

--- Codechef/238_makeEqual.cpp
+++ Codechef/238_makeEqual.cpp
@@ -1,28 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-int t;cin>>t;
-while(t--){
-  int a,b;
-  cin>>a>>b;
-  int m = max(a,b);
-  int n = min(a,b);
-  int flag = 0;
-  while(n<=m){
-    if(n==m){
-      cout<<"YES"<<endl;
-      flag = 1;
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int a, b;
+    cin >> a >> b;
+    int m = max(a, b);
+    int n = min(a, b);
+    int flag = 0;
+    while (n <= m)
+    {
+      if (n == m)
+      {
+        cout << "YES" << endl;
+        flag = 1;
+      }
+      n *= 2;
     }
-    n*=2;
+    if (flag == 0)
+    {
+      cout << "NO" << endl;
+    }
   }
-  if(flag==0){
-    cout<<"NO"<<endl;
-  }
-}
-return 0;
+  return 0;
 }
