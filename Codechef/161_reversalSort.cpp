bc40d004f83c2b4152ef12f28a357634bd208030
Gauri Bhand <htcgauribhand@gmail.com>
2022-08-16 21:52:29 +0530

    243

--- Codechef/161_reversalSort.cpp
+++ Codechef/161_reversalSort.cpp
@@ -1,34 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-int t;cin>>t;
-while(t--){
-  int b,x;
-  int a[b];
-  for(int i=0; i<b; i++){
-    cin>>a[i];
-  }
-  bool ans = true;
-  for(int i=0; i<b-1; i++){
-    if(a[i]>a[i+1]){
-        if(a[i]+a[i+1]<=x){
-            swap(a[i],a[i+1]);
-        }
-        else{
-            ans = false;
-            break;
-        }
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int b, x;
+    int a[b];
+    for (int i = 0; i < b; i++)
+    {
+      cin >> a[i];
     }
+    bool ans = true;
+    for (int i = 0; i < b - 1; i++)
+    {
+      if (a[i] > a[i + 1])
+      {
+        if (a[i] + a[i + 1] <= x)
+        {
+          swap(a[i], a[i + 1]);
+        }
+        else
+        {
+          ans = false;
+          break;
+        }
+      }
+    }
+    if (ans == true)
+      cout << "YES" << endl;
+    else
+      cout << "NO" << endl;
   }
-  if(ans==true)
-  cout<<"YES"<<endl;
-  else
-  cout<<"NO"<<endl;
-}
-return 0;
+  return 0;
 }
