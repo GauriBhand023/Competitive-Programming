4d7b385182b7cf09a252a3f6b6a467595979bed4
Gauri Bhand <htcgauribhand@gmail.com>
2022-08-16 21:50:54 +0530

    243

--- Codechef/231_medians.cpp
+++ Codechef/231_medians.cpp
@@ -1,36 +1,42 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-int n,a[1001],l,r;
+int n, a[1001], l, r;
 
-void sol(){
+void sol()
+{
     int n;
-    cin>>n;
-    a[1]=0;
-    l=0;
-    r=0;
-    for(int i=2; i<=n; ++i){
-        if(i%2==0){
-        a[i]=--l;
+    cin >> n;
+    a[1] = 0;
+    l = 0;
+    r = 0;
+    for (int i = 2; i <= n; ++i)
+    {
+        if (i % 2 == 0)
+        {
+            a[i] = --l;
         }
-        else{
-            a[i]=++r;
+        else
+        {
+            a[i] = ++r;
         }
     }
-    for(int i=1; i<=n; ++i)
-    cout<<a[i]-l+1<<" ";
-    cout<<endl;
-
+    for (int i = 1; i <= n; ++i)
+        cout << a[i] - l + 1 << " ";
+    cout << endl;
 }
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-int t;cin>>t;
-while(t--){
-    sol();
-  
-}
-return 0;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        sol();
+    }
+    return 0;
 }
