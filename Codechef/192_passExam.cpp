4dd094a945755a25d03f99cff78412d2a6323f53
Gauri Bhand <htcgauribhand@gmail.com>
2022-08-16 21:51:54 +0530

    243

--- Codechef/192_passExam.cpp
+++ Codechef/192_passExam.cpp
@@ -1,22 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-int t;cin>>t;
-while(t--){
-  int a,b,c;
-  cin>>a>>b>>c;
-  int sum = a+b+c;
-  if(sum>=100 && (a>=10 && b>=10 && c>=10)){
-    cout<<"Pass"<<endl;
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int a, b, c;
+    cin >> a >> b >> c;
+    int sum = a + b + c;
+    if (sum >= 100 && (a >= 10 && b >= 10 && c >= 10))
+    {
+      cout << "Pass" << endl;
+    }
+    else
+    {
+      cout << "Fail" << endl;
+    }
   }
-  else{
-    cout<<"Fail"<<endl;
-  }
-}
-return 0;
+  return 0;
 }
