e5a1976fae787889d676077d87366aed4aa42e1a
Gauri Bhand <htcgauribhand@gmail.com>
2022-08-17 21:08:59 +0530

    244

---
+++ Codechef/244_minSum.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int sum1(int a, int b){
+    if(b==0){
+        return a;
+    }
+    return sum1(b,a%b);
+}
+
+void sol(){
+    long long a,b,c;
+    cin>>a>>b;
+    for(int i=1; i<a; i++){
+        cin>>c;
+        b = sum1(b,c);
+    }
+    cout<<b*a<<endl;
+}
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  sol();
+}
+return 0;
+}
--- Codechef/245_WA.cpp
+++ Codechef/245_WA.cpp
@@ -2,23 +2,27 @@
 using namespace std;
 typedef long long ll;
 
-
  
 int main(){
 ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
 int t;cin>>t;
 while(t--){
-    int n;
+  int a = 120;
+  int n;
+  cin>>n;
   int arr[n];
-  int max = arr[0];
   for(int i=0; i<n; i++){
     cin>>arr[i];
-    if(arr[i]>max){
-        max = arr[i];
-    }
-    cout<<max<<endl;
   }
+  string s;
+  cin>>s;
+  for(int i=0; i<n; i++){
+    if(s[i]=='0'){
+        a = min(a,arr[i]);
+    }
+  }
+  cout<<a<<endl;
 }
 return 0;
 }
