715d054ce2306c131e2e62d69561af764538d2cc
Gauri Bhand <htcgauribhand@gmail.com>
2022-08-17 19:07:00 +0530

    243

--- Codechef/112_Dominant_Army.cpp
+++ Codechef/112_Dominant_Army.cpp
@@ -4,16 +4,19 @@ using namespace std;
 int main()
 {
     int d;
-    cin>>d;
-    while (d--) {  
-     int x,y,z;
-     cin>>x>>y>>z;
-     if(x>y+z || y>x+z || z>x+y){
-         cout<<"YES"<<endl;
-     }
-     else{
-         cout<<"NO"<<endl;
-     }
-  }    
-return 0;
+    cin >> d;
+    while (d--)
+    {
+        int x, y, z;
+        cin >> x >> y >> z;
+        if (x > y + z || y > x + z || z > x + y)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
 }
