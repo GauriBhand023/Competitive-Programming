dfdaa472075b22bad710630b3a443a0a0b14d903
Gauri Bhand <htcgauribhand@gmail.com>
2022-08-17 19:06:29 +0530

    243

--- Codechef/114_avgGift.cpp
+++ Codechef/114_avgGift.cpp
@@ -1,29 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-int t;
-cin>>t;
-while(t--){
-  int n,x;
-  cin>>n>>x;
-  int arr[n];
-  for(int i=0; i<n;i++){
-     cin>>arr[i];
-  }
-  int c =0;
-  for(int i=0; i<n; i++){
-      c=c+arr[i];
-  }
-  if(c/n==x){
-      cout<<"yes"<<endl;
-  }
-  else{
-      cout<<"no"<<endl;
-  }
-}
-return 0;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        int c = 0;
+        for (int i = 0; i < n; i++)
+        {
+            c = c + arr[i];
+        }
+        if (c / n == x)
+        {
+            cout << "yes" << endl;
+        }
+        else
+        {
+            cout << "no" << endl;
+        }
+    }
+    return 0;
 }
