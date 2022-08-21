86bdae8b8999eb7ca741b92f7667c7e73d3445b5
Gauri Bhand <htcgauribhand@gmail.com>
2022-08-21 09:05:21 +0530

    250

--- Codechef/245_WA.cpp
+++ Codechef/245_WA.cpp
@@ -1,28 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-int t;cin>>t;
-while(t--){
-  int a = 120;
-  int n;
-  cin>>n;
-  int arr[n];
-  for(int i=0; i<n; i++){
-    cin>>arr[i];
-  }
-  string s;
-  cin>>s;
-  for(int i=0; i<n; i++){
-    if(s[i]=='0'){
-        a = min(a,arr[i]);
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int a = 120;
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+      cin >> arr[i];
     }
+    string s;
+    cin >> s;
+    for (int i = 0; i < n; i++)
+    {
+      if (s[i] == '0')
+      {
+        a = min(a, arr[i]);
+      }
+    }
+    cout << a << endl;
   }
-  cout<<a<<endl;
-}
-return 0;
+  return 0;
 }
