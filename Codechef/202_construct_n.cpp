e8ae38dc223c47e3da04c65dc57d0caaab1a7bc8
Gauri Bhand <htcgauribhand@gmail.com>
2022-08-22 11:41:53 +0530

    252

--- Codechef/202_construct_n.cpp
+++ Codechef/202_construct_n.cpp
@@ -1,25 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
-int solve(int n){
-    cin>>n;
-    if(n<7 && n%2!=0){
-        cout<<"NO"<<endl;
+int solve(int n)
+{
+    cin >> n;
+    if (n < 7 && n % 2 != 0)
+    {
+        cout << "NO" << endl;
     }
-    else{
-        cout<<"YES"<<endl;
+    else
+    {
+        cout << "YES" << endl;
     }
- }
-
-
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
-int t;cin>>t;
-while(t--){
-    int a;
-    solve(a);
 }
-return 0;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a;
+        solve(a);
+    }
+    return 0;
 }
