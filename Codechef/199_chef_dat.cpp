a5d27c82b71d0da223ccda15542cad60239b8211
Gauri Bhand <htcgauribhand@gmail.com>
2022-08-22 11:45:37 +0530

    252

--- Codechef/199_chef_dat.cpp
+++ Codechef/199_chef_dat.cpp
@@ -1,24 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-int t;cin>>t;
-while(t--){
-  int a,b;
-  cin>>a>>b;
-  if(a==b){
-    cout<<"YES"<<endl;
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int a, b;
+    cin >> a >> b;
+    if (a == b)
+    {
+      cout << "YES" << endl;
+    }
+    else if (a < b)
+    {
+      cout << "NO" << endl;
+    }
+    else if (a > b)
+    {
+      cout << "YES" << endl;
+    }
   }
-  else if(a<b){
-    cout<<"NO"<<endl;
-  }
-  else if(a>b){
-    cout<<"YES"<<endl;
-  }
-}
-return 0;
+  return 0;
 }
