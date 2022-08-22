afb9e1a1491f5a880cbf531f8b96746560a01664
Gauri Bhand <htcgauribhand@gmail.com>
2022-08-22 11:42:29 +0530

    252

--- Codechef/201_mincoins.cpp
+++ Codechef/201_mincoins.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int t;
-    cin>>t;
-    while (t--) 
+    cin >> t;
+    while (t--)
     {
-       int x;
-       cin>>x;
-       cout<<x%10;
-        
+        int x;
+        cin >> x;
+        cout << x % 10;
     }
-    
+
     return 0;
 }
