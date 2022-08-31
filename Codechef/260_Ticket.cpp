549a55146f7f9abe9151f6f58a5fba267ccf6fd0
Gauri Bhand <htcgauribhand@gmail.com>
2022-08-31 20:53:15 +0530

    260

--- Codechef/260_Ticket.cpp
+++ Codechef/260_Ticket.cpp
@@ -2,21 +2,15 @@
 using namespace std;
 typedef long long ll;
 
+ 
 int main(){
 ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
 int t;cin>>t;
 while(t--){
-  int a,b;
-  if(a==b){
-    cout<<"Yes"<<endl;
-  }
-  else if(abs(a-b)%2==0){
-    cout<<"Yes"<<endl;
-  }
-  else{
-    cout<<"No"<<endl;
-  }
+  int a,b,c;
+  cin>>a>>b>>c;
+  cout<<(b-c)*a<<endl;
 }
 return 0;
 }
