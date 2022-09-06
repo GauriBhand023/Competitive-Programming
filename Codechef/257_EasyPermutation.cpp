688c549f8f2afb2b130cdde319c43a5f2f79f77e
Gauri Bhand <htcgauribhand@gmail.com>
2022-09-06 14:37:48 +0530

    257

--- Codechef/257_EasyPermutation.cpp
+++ Codechef/257_EasyPermutation.cpp
@@ -1,19 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-int t;cin>>t;
-while(t--){
-  int a;
-  cin>>a;
-  for(int i=a; i>=1; i--){
-    cout<<i<<" ";
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int a;
+    cin >> a;
+    for (int i = a; i >= 1; i--)
+    {
+      cout << i << " ";
+    }
+    cout << endl;
   }
-  cout<<endl;
-}
-return 0;
+  return 0;
 }
