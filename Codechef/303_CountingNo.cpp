40717f2b112a4f46a39cd21448f92eb69d892c6f
Gauri Bhand <htcgauribhand@gmail.com>
2022-12-01 14:40:17 +0530

    305

--- Codechef/303_CountingNo.cpp
+++ Codechef/303_CountingNo.cpp
@@ -1,26 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-int t;cin>>t;
-while(t--){
-  int a;
-  cin>>a;
-  int arr[a];
-  int cnt=0;
-  for(int i=0; i<a; i++){
-    cin>>arr[i];
-    if(arr[i]%2)
-    cnt++;
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int a;
+    cin >> a;
+    int arr[a];
+    int cnt = 0;
+    for (int i = 0; i < a; i++)
+    {
+      cin >> arr[i];
+      if (arr[i] % 2)
+        cnt++;
+    }
+    if (cnt == 0 || cnt % 2)
+      cout << "NO" << endl;
+    else
+      cout << "YES" << endl;
   }
-  if(cnt==0 || cnt%2)
-  cout<<"NO"<<endl;
-  else
-  cout<<"YES"<<endl;
-}
-return 0;
+  return 0;
 }
