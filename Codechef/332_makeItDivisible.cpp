0b79f846921cefd6e5d8acfd4b3ce7df5c30620e
Gauri Bhand <htcgauribhand@gmail.com>
2022-12-15 17:27:16 +0530

    332

---
+++ Codechef/332_makeItDivisible.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n] = {0};
+        a[0] = 3;
+        a[n - 1] = 3;
+        for (int i = 0; i < n; i++)
+        {
+            cout << a[i];
+        }
+        cout << endl;
+    }
+    return 0;
+}
