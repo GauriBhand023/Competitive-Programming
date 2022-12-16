723a7b331cf37c4c9e08e87ac56dd64fd3b75b55
Gauri Bhand <htcgauribhand@gmail.com>
2022-12-16 10:41:49 +0530

    333

---
+++ Codechef/333_indivisiblePermutation.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main() {
+	int t;cin>>t;
+	while(t--){
+	    int n;cin>>n;
+	    cout<<n<<" ";
+	    for(int i=1;i<n;i++) cout<<i<<" ";
+	    cout<<endl;
+	    
+	}
+	return 0;
+}
