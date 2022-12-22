f94a7a61c6a880cb37e60952f5f5cd3d895b1ff6
Gauri Bhand <htcgauribhand@gmail.com>
2022-12-22 21:13:22 +0530

    11

---
+++ LEETCODE/11_balanced_binary_tree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   int height(TreeNode *node)
+	{
+		if(node==NULL) return 0;
+    
+    return 1+max(height(node->left),height(node->right));
+}
+    bool isBalanced(TreeNode* root) {
+        if(root==NULL) return true;
+    
+    int lh=height(root->left);
+    int rh=height(root->right);
+    
+    return (abs(lh-rh)<=1 && isBalanced(root->left) && isBalanced(root->right));
+    }
+};
