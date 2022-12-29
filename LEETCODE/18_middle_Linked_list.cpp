39712adfbf5947c47a6038c17ed62ed7e202fac8
Gauri Bhand <htcgauribhand@gmail.com>
2022-12-29 22:03:42 +0530

    18

---
+++ LEETCODE/18_middle_Linked_list.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* p1 = head;
+        ListNode* p2 = head;
+        while(p2!=NULL && p2->next!=NULL){
+            p1=p1->next;
+            p2=p2->next->next;
+        }
+        return p1;
+    }
+};
