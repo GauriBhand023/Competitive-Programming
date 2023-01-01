eb7f1af039386fa9f626f1f507feff51fe1c8cb6
Gauri Bhand <htcgauribhand@gmail.com>
2023-01-01 23:30:57 +0530

    19

---
+++ LEETCODE/20_word_pattern.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        
+        if(pattern==s){
+            if(pattern.size()==1){
+                return true;
+               
+            }
+            return false;
+        }
+        int n=s.length();
+        vector<string> words;
+        string t="";
+        for(int i=0; i<=s.size(); i++) {
+            if(s[i] == '\0') {
+                words.push_back(t);
+                break;
+            }
+            else if(s[i] == ' ') {
+                words.push_back(t);
+                t="";
+            }
+            else t+=s[i];
+            
+        }
+        int a=words.size();
+        if(pattern.size() != words.size()){
+            return false;
+        }
+        for(int i=0;i<a;i++){
+            for(int j=0;j<a;j++){
+                if(words[i]==words[j] && pattern[i]!=pattern[j]){
+                    return false;
+                }
+                if(pattern[i]==pattern[j] && words[i]!=words[j]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
